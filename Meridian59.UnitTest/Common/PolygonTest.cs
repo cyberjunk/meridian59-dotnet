6757c9bacb7cc989bb330678354c609d51e31a41
Clint Banzhaf <clint.banzhaf@gmx.net>
2015-04-28 19:52:53 +0200

    Polygon stuff

--- Meridian59.UnitTest/Common/PolygonTest.cs
+++ Meridian59.UnitTest/Common/PolygonTest.cs
@@ -297,5 +297,276 @@ namespace Meridian59.UnitTest
 
             Assert.AreEqual(expected, returned);
         }
+
+        /// <summary>
+        /// Test for SplitConvexPolygon()
+        /// </summary>
+        [TestMethod]
+        public void SplitConvexPolygon()
+        {
+            Polygon poly = new Polygon();
+            Polygon expectedLeft = new Polygon();
+            Polygon expectedRight = new Polygon();
+            V2 p1, p2;
+
+            Tuple<Polygon, Polygon> returned;
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // intersect nothing from south to north
+            //                  |
+            //   B----------C   |
+            //   |          |   |
+            //   |          |   |
+            //   |          |   |
+            //   A----------D   |
+            //                  |
+            p1 = new V2(20.5f, -15f);
+            p2 = new V2(20.5f, 15);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D
+            // right empty
+            // left = old
+            expectedLeft.Add(new V2(0.0f, 0.0f));
+            expectedLeft.Add(new V2(0.0f, 5.0f));
+            expectedLeft.Add(new V2(5.0f, 5.0f));
+            expectedLeft.Add(new V2(5.0f, 0.0f));
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // intersect nothing from north to south
+            //                  |
+            //   B----------C   |
+            //   |          |   |
+            //   |          |   |
+            //   |          |   |
+            //   A----------D   |
+            //                  |
+            p1 = new V2(20.5f, 15f);
+            p2 = new V2(20.5f, -15);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D
+            // right = old
+            expectedRight.Add(new V2(0.0f, 0.0f));
+            expectedRight.Add(new V2(0.0f, 5.0f));
+            expectedRight.Add(new V2(5.0f, 5.0f));
+            expectedRight.Add(new V2(5.0f, 0.0f));
+            // left empty
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // intersect edges BC, DA from south to north
+            //        |
+            //   B----I1----C
+            //   |    |     |
+            //   |    |     |
+            //   |    |     |
+            //   A----I2----D
+            //        |
+            p1 = new V2(2.5f, -10f);
+            p2 = new V2(2.5f, 10);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();           
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D
+            expectedRight.Add(new V2(2.5f, 5.0f));
+            expectedRight.Add(new V2(5.0f, 5.0f));
+            expectedRight.Add(new V2(5.0f, 0.0f));
+            expectedRight.Add(new V2(2.5f, 0.0f));
+            expectedLeft.Add(new V2(2.5f, 0.0f));
+            expectedLeft.Add(new V2(0.0f, 0.0f));
+            expectedLeft.Add(new V2(0.0f, 5.0f));
+            expectedLeft.Add(new V2(2.5f, 5.0f));
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // intersect edges BC, DA from nort to south
+            //        |
+            //   B----I1----C
+            //   |    |     |
+            //   |    |     |
+            //   |    |     |
+            //   A----I2----D
+            //        |
+            p1 = new V2(2.5f, 10f);
+            p2 = new V2(2.5f, -10);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D
+            expectedLeft.Add(new V2(2.5f, 5.0f));
+            expectedLeft.Add(new V2(5.0f, 5.0f));
+            expectedLeft.Add(new V2(5.0f, 0.0f));
+            expectedLeft.Add(new V2(2.5f, 0.0f));
+            expectedRight.Add(new V2(2.5f, 0.0f));
+            expectedRight.Add(new V2(0.0f, 0.0f));
+            expectedRight.Add(new V2(0.0f, 5.0f));
+            expectedRight.Add(new V2(2.5f, 5.0f));
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // 1 boundary point at C with splitter from north-west to south-east
+            //             \
+            //   B----------C
+            //   |          |\
+            //   |          | \
+            //   |          |  \
+            //   A----------D
+            //         
+            p1 = new V2(4.0f, 6.0f);
+            p2 = new V2(6.0f, 4.0f);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D
+            // left empty
+            // right old
+            expectedRight.Add(new V2(0.0f, 0.0f));
+            expectedRight.Add(new V2(0.0f, 5.0f));
+            expectedRight.Add(new V2(5.0f, 5.0f));
+            expectedRight.Add(new V2(5.0f, 0.0f));
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // edge CD coincide with splitter from south to north
+            //              |
+            //   B----------C
+            //   |          |
+            //   |          |
+            //   |          |
+            //   A----------D
+            //              |
+            p1 = new V2(5.0f, -10.0f);
+            p2 = new V2(5.0f, 3.0f);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D          
+            // right empty
+            // left old
+            expectedLeft.Add(new V2(0.0f, 0.0f));
+            expectedLeft.Add(new V2(0.0f, 5.0f));
+            expectedLeft.Add(new V2(5.0f, 5.0f));
+            expectedLeft.Add(new V2(5.0f, 0.0f));
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // two boundary points at vertices A and C
+            // by diagonal infinite line AC
+            //         /
+            //   B----C
+            //   |   /|
+            //   |  / |
+            //   | /  |
+            //   |/   |
+            //   A----D
+            //  /         
+            p1 = new V2(-1.0f, -1.0f);
+            p2 = new V2(2.0f, 2.0f);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D          
+            expectedRight.Add(new V2(5.0f, 5.0f)); // C
+            expectedRight.Add(new V2(5.0f, 0.0f)); // D
+            expectedRight.Add(new V2(0.0f, 0.0f)); // A
+            expectedLeft.Add(new V2(0.0f, 0.0f)); // A
+            expectedLeft.Add(new V2(0.0f, 5.0f)); // B
+            expectedLeft.Add(new V2(5.0f, 5.0f)); // C
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+
+            // --- TEST ---
+            // 4 points, valid clockwise rectangle ABCD
+            // one intersection at I1, one boundarypoint A
+
+            //         /
+            //   B----I1--C
+            //   |   /    |
+            //   |  /     |
+            //   | /      |
+            //   |/       |
+            //   A--------D
+            //  /         
+            p1 = new V2(-0.5f, -1.0f);
+            p2 = new V2(0.5f, 1.0f);
+            poly.Clear();
+            expectedLeft.Clear();
+            expectedRight.Clear();
+            poly.Add(new V2(0.0f, 0.0f)); // A
+            poly.Add(new V2(0.0f, 5.0f)); // B
+            poly.Add(new V2(5.0f, 5.0f)); // C
+            poly.Add(new V2(5.0f, 0.0f)); // D
+            expectedRight.Add(new V2(2.5f, 5.0f)); // I1
+            expectedRight.Add(new V2(5.0f, 5.0f)); // C
+            expectedRight.Add(new V2(5.0f, 0.0f)); // D
+            expectedRight.Add(new V2(0.0f, 0.0f)); // A
+            expectedLeft.Add(new V2(0.0f, 0.0f)); // A
+            expectedLeft.Add(new V2(0.0f, 5.0f)); // B
+            expectedLeft.Add(new V2(2.5f, 5.0f)); // I1
+
+            returned = poly.SplitConvexPolygon(p1, p2);
+
+            Assert.AreEqual(returned.Item1.IsIdentical(expectedRight), true);
+            Assert.AreEqual(returned.Item2.IsIdentical(expectedLeft), true);
+        }
     }
 }
--- Meridian59/Common/Polygon.cs
+++ Meridian59/Common/Polygon.cs
@@ -32,6 +32,59 @@ namespace Meridian59.Common
     public class Polygon : List<V2>
     {
         /// <summary>
+        /// Returns true if two polygons are exactly identical.
+        /// Note: The ordering/start of points also matters here.
+        /// If two polygons contain the same points but indices are shifted,
+        /// the return would be a false.
+        /// </summary>
+        /// <param name="Polygon"></param>
+        /// <returns></returns>
+        public bool IsIdentical(Polygon Polygon)
+        {
+            // no points
+            if (Polygon.Count == 0 && Count == 0)
+                return true;
+
+            // not same amount of points
+            if (Polygon.Count != Count)
+                return false;
+
+            // points must match exactly
+            for (int i = 0; i < Count; i++)
+                if (this[i] != Polygon[i])
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns true if two polygons contain the
+        /// same points. The ordering does not matter.
+        /// Note: This can return true for two polygons,
+        /// which use the same points, but have different shape
+        /// due to different ordering of these points.
+        /// </summary>
+        /// <param name="Polygon"></param>
+        /// <returns></returns>
+        public bool HasSamePoints(Polygon Polygon)
+        {
+            // no points
+            if (Polygon.Count == 0 && Count == 0)
+                return true;
+
+            // not same amount of points
+            if (Polygon.Count != Count)
+                return false;
+
+            // point must be existant in both polys
+            for (int i = 0; i < Count; i++)
+                if (!this.Contains(Polygon[i]))
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
         /// True if the first three points of this polygon
         /// describe a valid triangle.
         /// </summary>
@@ -111,38 +164,115 @@ namespace Meridian59.Common
             return removed;
         }
 
-        /*public Tuple<Polygon, Polygon> SplitConvexPolygon(V2 P1, V2 P2)
+        /// <summary>
+        /// Searches for the first edge the new Point lies on.
+        /// If it's not a polygon vertex, it's added at according index.
+        /// </summary>
+        /// <param name="Point"></param>
+        /// <returns>True if the point was added.</returns>
+        public bool AddPointOnEdge(V2 Point)
+        {
+            // walk the poly edges
+            for (int i = 0; i < Count - 1; i++)
+            {
+                if (Point.IsOnLineSegment(this[i], this[i + 1]) &&
+                    Point != this[i] && Point != this[i+1])
+                {
+                    this.Insert(i + 1, Point);                   
+                    return true;
+                }
+            }
+
+            // on edge between last and first point
+            if (Point.IsOnLineSegment(this[Count-1], this[0]) &&
+                Point != this[Count-1] && Point != this[0])
+            {
+                this.Add(Point);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Adds points from this polygon starting at FromIndex up to including ToIndex
+        /// into a Target Polygon. If FromIndex is bigger than ToIndex, it will iterate
+        /// over the zero index (2 chunks: FromIndex->end, 0->ToIndex).
+        /// </summary>
+        /// <param name="FromIndex"></param>
+        /// <param name="ToIndex"></param>
+        /// <param name="Target"></param>
+        protected void AddIndexRangeToOtherPolygon(int FromIndex, int ToIndex, Polygon Target)
+        {
+            // seamless range
+            if (FromIndex < ToIndex)
+            {
+                for (int i = FromIndex; i <= ToIndex; i++)
+                    Target.Add(this[i]);
+            }
+        
+            // splitted range
+            else if (FromIndex > ToIndex)
+            {
+                // from last to end
+                for (int i = FromIndex; i < Count; i++)
+                    Target.Add(this[i]);
+
+                // from 0 to first
+                for (int i = 0; i <= ToIndex; i++)
+                    Target.Add(this[i]);
+            }
+        }
+
+        /// <summary>
+        /// Splits this polygon using a infinite line given by P1 (start) and P2 (end).
+        /// Will check for convexity first.
+        /// </summary>
+        /// <param name="P1"></param>
+        /// <param name="P2"></param>
+        /// <returns>
+        /// Null in case the polygon is not convex or something went wrong.
+        /// A tuple with Item1 (right) and Item2 (left) polygon.
+        /// </returns>
+        public Tuple<Polygon, Polygon> SplitConvexPolygon(V2 P1, V2 P2)
         {
             // make sure this is a convex polygon
             if (!IsConvexPolygon())
                 return null;
+            
+            /***************************************************************/
 
+            Polygon poly;
             V2 intersect;
             LineInfiniteLineIntersectionType intersecttype;
             List<V2> intersections  = new List<V2>();
             List<V2> boundarypoints = new List<V2>();
             int numcoincides      = 0;
-            
+            int side;
+
             // create left and right polygons
             Polygon polyRight = new Polygon();
             Polygon polyLeft = new Polygon();
-            
-            Tuple<Polygon, Polygon> splitted = 
-                new Tuple<Polygon, Polygon>(polyRight, polyLeft);
+
+            /***************************************************************/
 
             // test intersection of all polygon edges (finite lines) with infinite line p1p2
             for (int i = 0; i < Count - 1; i++)
             {
                 intersecttype = MathUtil.IntersectLineInfiniteLine(this[i], this[i + 1], P1, P2, out intersect);
 
-                // count intersection
-                if (intersecttype == LineInfiniteLineIntersectionType.OneIntersection)
-                    intersections.Add(intersect);
-
-                // count boundarypoints (will be counted twice, for each of the two edges)
-                else if (intersecttype == LineInfiniteLineIntersectionType.OneBoundaryPoint)
-                    boundarypoints.Add(intersect);
-
+                // intersection
+                if (intersecttype == LineInfiniteLineIntersectionType.OneIntersection &&
+                    !intersections.Contains(intersect))               
+                        intersections.Add(intersect);
+                
+                // boundarypoint
+                // note: in case we tried to add a boundarypoint which is already there
+                // we in fact have a polygon vertex intersection, could add it there and change handling..
+                else if (intersecttype == LineInfiniteLineIntersectionType.OneBoundaryPoint &&
+                    !boundarypoints.Contains(intersect))               
+                        boundarypoints.Add(intersect);
+                
                 // count coincides
                 else if (intersecttype == LineInfiniteLineIntersectionType.FullyCoincide)
                     numcoincides++;
@@ -150,63 +280,127 @@ namespace Meridian59.Common
 
             // also test the edge from last point to first point
             intersecttype = MathUtil.IntersectLineInfiniteLine(this[Count-1], this[0], P1, P2, out intersect);
-            if (intersecttype == LineInfiniteLineIntersectionType.OneIntersection)
-                intersections.Add(intersect);
-            else if (intersecttype == LineInfiniteLineIntersectionType.OneBoundaryPoint)
-                boundarypoints.Add(intersect);
+            if (intersecttype == LineInfiniteLineIntersectionType.OneIntersection &&
+                !intersections.Contains(intersect))
+                    intersections.Add(intersect);
+            else if (intersecttype == LineInfiniteLineIntersectionType.OneBoundaryPoint &&
+                !boundarypoints.Contains(intersect))
+                    boundarypoints.Add(intersect);
             else if (intersecttype == LineInfiniteLineIntersectionType.FullyCoincide)
                 numcoincides++;
+
+            /***************************************************************/
+          
+            bool case1 = 
+                (numcoincides == 0 && intersections.Count == 0 && boundarypoints.Count == 0) ||
+                (numcoincides == 1 && intersections.Count == 0 && boundarypoints.Count == 2) ||
+                (numcoincides == 0 && intersections.Count == 0 && boundarypoints.Count == 1);
+
+            bool case2a = 
+                (numcoincides == 0 && intersections.Count == 2 && boundarypoints.Count == 0);
+
+            bool case2b =
+                (numcoincides == 0 && intersections.Count == 0 && boundarypoints.Count == 2);
+
+            bool case2c = 
+                (numcoincides == 0 && intersections.Count == 1 && boundarypoints.Count == 1);
 
             // case (1): all on one side
             // (a) no intersection, no boundary point and no coincide edge (trivial)
             // (b) one edge is coincide with splitter
-            //     =two edge-endpoints also touch it, endpoints of coincide edge are not counted
-            // (c) exactly one polygon point is a boundarypoint on the splitter (both edge endpoints counted
-            if ((numcoincides == 0 && intersections.Count == 0 && boundarypoints.Count == 0) ||
-                (numcoincides == 1 && intersections.Count == 0 && boundarypoints.Count == 2) ||
-                (numcoincides == 0 && intersections.Count == 0 && boundarypoints.Count == 2))
+            // (c) exactly one polygon point is a boundarypoint on the splitter
+            if (case1)
             {
                 // determine side by finding first point not on splitter
-                for (int i = 0; i < Count - 1; i++)
+                for (int i = 0; i < Count; i++)
                 {
-                    int side = this[i].GetSide(P1, P2);
+                    side = this[i].GetSide(P1, P2);
 
                     // add verything to right poly and return
-                    if (side > 0)
+                    if (side < 0)
                     {
                         polyRight.AddRange(this);
-                        return splitted;
+                        return new Tuple<Polygon, Polygon>(polyRight, polyLeft);
                     }
 
                     // add everything to left poly and return
-                    else if (side < 0)
+                    else if (side > 0)
                     {
                         polyLeft.AddRange(this);
-                        return splitted;
+                        return new Tuple<Polygon, Polygon>(polyRight, polyLeft);
                     }
                 }
+
+                return null; // WTF?
             }
 
-            // case (2): infinite line intersects two polygon edges
-            else if (numcoincides == 0 && intersections.Count == 2 && boundarypoints.Count == 0)
+            // case (2)
+            else if (case2a || case2b || case2c)
             {
-                // split up
-            }
+                int idx  = -1;
+                int idx1 = -1;
+                int idx2 = -1;
 
-            // case (3): infinite line intersects two polygon vertices (4 endpoints lie on splitter)
-            else if (numcoincides == 0 && intersections.Count == 0 && boundarypoints.Count == 4)
-            {
-                // split up
-            }
+                // case (2a): infinite line intersects two polygon edges
+                // -> add both intersection vertices, then use them
+                if (case2a)
+                {
+                    AddPointOnEdge(intersections[0]);
+                    AddPointOnEdge(intersections[1]);
+                    idx1 = IndexOf(intersections[0]);
+                    idx2 = IndexOf(intersections[1]);
+                }
 
-            // case (3): infinite line intersects one polygon edge and one vertex (2 endpoints there)
-            else if (numcoincides == 0 && intersections.Count == 1 && boundarypoints.Count == 2)
-            {
-                // split up
-            }
+                // case (2b): infinite line intersects two poly vertices
+                // -> use them directly
+                else if (case2b)
+                {
+                    idx1 = IndexOf(boundarypoints[0]);
+                    idx2 = IndexOf(boundarypoints[1]);
+                }
 
-            // something wrong
-            return null;
-        }*/
+                // case (2c): infinite line intersects one polygon edge and one vertex
+                else if (case2c)
+                {
+                    AddPointOnEdge(intersections[0]);
+                    idx1 = IndexOf(intersections[0]);
+                    idx2 = IndexOf(boundarypoints[0]);
+                }
+
+                if (idx1 < 0 || idx2 < 0 || idx1 == idx2)
+                    return null; // WTF?
+
+                // now add points to each poly
+
+                // poly 1
+                // use side of next point
+                idx = (idx1 + 1 < Count) ? idx1 + 1 : 0;
+                side = this[idx].GetSide(P1, P2);
+
+                if (side < 0) poly = polyRight;
+                else if (side > 0) poly = polyLeft;
+                else
+                    return null; // WTF?
+
+                AddIndexRangeToOtherPolygon(idx1, idx2, poly);
+
+                // poly 2
+                // use side of next point
+                idx = (idx2 + 1 < Count) ? idx2 + 1 : 0;
+                side = this[idx].GetSide(P1, P2);
+
+                if (side < 0) poly = polyRight;
+                else if (side > 0) poly = polyLeft;
+                else
+                    return null; // WTF?
+
+                AddIndexRangeToOtherPolygon(idx2, idx1, poly);
+
+                // return both new polys
+                return new Tuple<Polygon, Polygon>(polyRight, polyLeft);
+            }
+            else
+                return null; // WTF?
+        }
     }
 }
