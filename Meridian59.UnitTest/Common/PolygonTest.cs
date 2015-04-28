5247fd10c202164cb80220341e3b5ee8ddd55925
Clint Banzhaf <clint.banzhaf@gmx.net>
2015-04-28 05:35:29 +0200

    Basic Polygon class

---
+++ Meridian59.UnitTest/Common/PolygonTest.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Meridian59.Common;
+
+// Switch FP precision based on architecture
+#if X64
+using Real = System.Double;
+#else
+using Real = System.Single;
+#endif
+
+namespace Meridian59.UnitTest
+{
+    /// <summary>
+    /// Tests for Common.V2
+    /// </summary>
+    [TestClass]
+    public class PolygonTest
+    {
+        const Real EPSILON = 0.0000001f;
+
+        /// <summary>
+        /// Test for IsPolygon()
+        /// </summary>
+        [TestMethod]
+        public void IsPolygon()
+        {
+            Polygon poly = new Polygon();
+
+            bool expected;
+            bool returned;
+
+            // --- TEST ---
+            // no points
+            poly.Clear();
+
+            expected = false;
+            returned = poly.IsPolygon();
+            
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 1 point
+            poly.Clear();
+            poly.Add(new V2(35.0f, 2.0f));
+
+            expected = false;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 2 points
+            poly.Clear();
+            poly.Add(new V2(35.0f, 2.0f));
+            poly.Add(new V2(365.0f, 23.0f));
+
+            expected = false;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 3 points all equal
+            poly.Clear();
+            poly.Add(new V2(35.0f, 2.0f));
+            poly.Add(new V2(35.0f, 2.0f));
+            poly.Add(new V2(35.0f, 2.0f));
+
+            expected = false;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 3 points, 1&3 equal
+            poly.Clear();
+            poly.Add(new V2(35.0f, 2.0f));
+            poly.Add(new V2(3.0f, 6.0f));
+            poly.Add(new V2(35.0f, 2.0f));
+
+            expected = false;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 3 points, 1&2 equal
+            poly.Clear();
+            poly.Add(new V2(3.0f, 6.0f));
+            poly.Add(new V2(3.0f, 6.0f));
+            poly.Add(new V2(35.0f, 2.0f));
+
+            expected = false;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 3 points, 2&3 equal
+            poly.Clear();
+            poly.Add(new V2(35.0f, 2.0f));
+            poly.Add(new V2(3.0f, 6.0f));
+            poly.Add(new V2(3.0f, 6.0f));
+
+            expected = false;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 3 points on same line
+            poly.Clear();
+            poly.Add(new V2(5.0f, 7.0f));
+            poly.Add(new V2(5.0f, 5.0f));
+            poly.Add(new V2(5.0f, 10.0f));
+
+            expected = false;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 3 points, valid triangle
+            poly.Clear();
+            poly.Add(new V2(10.0f, 10.0f));
+            poly.Add(new V2(5.0f, 0.0f));
+            poly.Add(new V2(10.0f, -10.0f));
+
+            expected = true;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 4 points, rectangle ordered
+            poly.Clear();
+            poly.Add(new V2(0.0f, 0.0f));
+            poly.Add(new V2(0.0f, 5.0f));
+            poly.Add(new V2(5.0f, 5.0f));
+            poly.Add(new V2(5.0f, 0.0f));
+
+            expected = true;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+
+            // --- TEST ---
+            // 4 points, no rectangle order, but still poly
+            poly.Clear();
+            poly.Add(new V2(0.0f, 0.0f));
+            poly.Add(new V2(5.0f, 5.0f));
+            poly.Add(new V2(0.0f, 5.0f));
+            poly.Add(new V2(5.0f, 0.0f));
+
+            expected = true;
+            returned = poly.IsPolygon();
+
+            Assert.AreEqual(expected, returned);
+        }
+    }
+}
--- Meridian59/Common/Polygon.cs
+++ Meridian59/Common/Polygon.cs
@@ -14,7 +14,15 @@
  If not, see http://www.gnu.org/licenses/.
 */
 
+using System;
 using System.Collections.Generic;
+
+// Switch FP precision based on architecture
+#if X64
+using Real = System.Double;
+#else
+using Real = System.Single;
+#endif
 
 namespace Meridian59.Common
 {
@@ -23,5 +31,19 @@ namespace Meridian59.Common
     /// </summary>
     public class Polygon : List<V2>
     {
+        /// <summary>
+        /// True if this polygon has at least 3 different points.
+        /// None of them equal or on the line given by other two.
+        /// Note: Points must be ordered.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPolygon()
+        {
+            return
+                Count >= 3 &&
+                !this[0].IsOnLineSegment(this[1], this[2]) &&
+                !this[1].IsOnLineSegment(this[0], this[2]) &&
+                !this[2].IsOnLineSegment(this[0], this[1]);
+        }
     }
 }
