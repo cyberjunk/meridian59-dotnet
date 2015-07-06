506f2de2e56b949c092915de6285332e02a95890
Clint Banzhaf <clint.banzhaf@gmx.net>
2015-07-07 01:06:47 +0200

    Increase versions

--- Meridian59.DebugUI/Properties/AssemblyInfo.cs
+++ Meridian59.DebugUI/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("175ff980-037a-49ab-b948-ad58abd74716")]
 
-[assembly: AssemblyVersion("1.2.2.2")]
-[assembly: AssemblyFileVersion("1.2.2.2")]
+[assembly: AssemblyVersion("1.2.2.3")]
+[assembly: AssemblyFileVersion("1.2.2.3")]
