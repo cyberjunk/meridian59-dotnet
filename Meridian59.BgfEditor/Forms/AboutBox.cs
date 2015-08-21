2579596db59c42dac2db7b688b7475ce7df5b16f
Clint Banzhaf <clint.banzhaf@gmx.net>
2015-08-21 07:30:58 +0200

    BgfEditor: Also check for crush32.dll in system32 folder

--- Meridian59.BgfEditor/Forms/AboutBox.cs
+++ Meridian59.BgfEditor/Forms/AboutBox.cs
@@ -1,4 +1,6 @@
 ﻿using Meridian59.Files.BGF;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Meridian59.BgfEditor
@@ -11,7 +13,9 @@ namespace Meridian59.BgfEditor
 
             lblVersion.Text = BgfFile.VERSION9.ToString() + " / " + BgfFile.VERSION10.ToString();
 
-            if (System.IO.File.Exists("crush32.dll"))
+            // check for crush32.dll either in program directory or system32 (syswow64)
+            if (System.IO.File.Exists("crush32.dll") ||
+                System.IO.File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86), "crush32.dll")))
             {
                 txtCrush.Text = "AVAILABLE";
             }
