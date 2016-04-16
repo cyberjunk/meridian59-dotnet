1f733bf297ddf0ff40cf5b678feb4a12e02e1af8
Clint Banzhaf <clint.banzhaf@gmx.net>
2016-04-16 23:31:16 +0200

    Undo last fix for event after destroy (see CEGUI fix)

--- Meridian59.Ogre.Client/UIOptions.cpp
+++ Meridian59.Ogre.Client/UIOptions.cpp
@@ -807,20 +807,6 @@ namespace Meridian59 { namespace Ogre
 		// detach listener from clientpreferences
 		OgreClient::Singleton->Data->ClientPreferences->PropertyChanged -=
 			gcnew PropertyChangedEventHandler(OnClientPreferencesPropertyChanged);
-
-		// detach CEGUI event listeners
-		size_t numAliases = ListAliases->getItemCount();
-		for (size_t i = 0; i < numAliases; i++)
-		{
-			CEGUI::Window* widget	= ListAliases->getItemFromIndex(i);
-			CEGUI::PushButton* del	= (CEGUI::PushButton*)widget->getChildAtIdx(UI_OPTIONS_CHILDINDEX_ALIAS_DELETE);
-			CEGUI::Editbox* key		= (CEGUI::Editbox*)widget->getChildAtIdx(UI_OPTIONS_CHILDINDEX_ALIAS_KEY);
-			CEGUI::Editbox* value	= (CEGUI::Editbox*)widget->getChildAtIdx(UI_OPTIONS_CHILDINDEX_ALIAS_VALUE);
-
-			del->removeAllEvents();
-			key->removeAllEvents();
-			value->removeAllEvents();
-		}
 	};
 
 	void ControllerUI::Options::OnConfigPropertyChanged(Object^ sender, PropertyChangedEventArgs^ e)
