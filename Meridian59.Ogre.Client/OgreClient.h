889b6e6f936de73ab1bb3c445c1244c8a5bef1ab
Clint Banzhaf <clint.banzhaf@gmx.net>
2016-06-21 21:59:55 +0200

    Increment OgreClient internal version to 90.8

--- Meridian59.Ogre.Client/OgreClient.h
+++ Meridian59.Ogre.Client/OgreClient.h
@@ -222,7 +222,7 @@ namespace Meridian59 { namespace Ogre
 
 		property unsigned char AppVersionMinor
 		{ 
-			public: virtual unsigned char get() override { return 7; } 			
+			public: virtual unsigned char get() override { return 8; } 			
 		};
 		
 		property ::Ogre::Root* Root 
