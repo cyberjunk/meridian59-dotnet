e27d19b78f6cd0f1e732b0cf0575e9130aae1a05
Morbus <fonkykitten@gmail.com>
2016-09-09 17:50:36 +0200

    Modified default keybindings to match classical client more.

--- Meridian59.Ogre.Client/OISKeyBinding.cpp
+++ Meridian59.Ogre.Client/OISKeyBinding.cpp
@@ -26,80 +26,80 @@ namespace Meridian59 { namespace Ogre
 
 	OISKeyBinding^ OISKeyBinding::GetDefault()
     {       
-        OISKeyBinding^ defaultBinding = gcnew OISKeyBinding();
+      OISKeyBinding^ defaultBinding = gcnew OISKeyBinding();
 
-		defaultBinding->RightClickAction = 13;
+      defaultBinding->RightClickAction = 48;
 
-        // Movement
-        defaultBinding->MoveForward		= ::OIS::KeyCode::KC_W;
-        defaultBinding->MoveBackward	= ::OIS::KeyCode::KC_S;
-        defaultBinding->MoveLeft		= ::OIS::KeyCode::KC_Q;
-        defaultBinding->MoveRight		= ::OIS::KeyCode::KC_E;
+      // Movement
+      defaultBinding->MoveForward		= ::OIS::KeyCode::KC_W;
+      defaultBinding->MoveBackward	= ::OIS::KeyCode::KC_S;
+      defaultBinding->MoveLeft		= ::OIS::KeyCode::KC_A;
+      defaultBinding->MoveRight		= ::OIS::KeyCode::KC_D;
 
-        // Rotation
-        defaultBinding->RotateLeft		= ::OIS::KeyCode::KC_A;
-        defaultBinding->RotateRight		= ::OIS::KeyCode::KC_D;
+      // Rotation
+      defaultBinding->RotateLeft		= ::OIS::KeyCode::KC_LEFT;
+      defaultBinding->RotateRight		= ::OIS::KeyCode::KC_RIGHT;
 
-        // Modifiers
-        defaultBinding->Walk			= ::OIS::KeyCode::KC_LSHIFT;
-        defaultBinding->AutoMove		= ::OIS::KeyCode::KC_NUMLOCK;
-        
-        // Targetting
-        defaultBinding->NextTarget		= ::OIS::KeyCode::KC_TAB;
-        defaultBinding->SelfTarget		= ::OIS::KeyCode::KC_LMENU;
+      // Modifiers
+      defaultBinding->Walk			= ::OIS::KeyCode::KC_LSHIFT;
+      defaultBinding->AutoMove		= ::OIS::KeyCode::KC_NUMLOCK;
 
-        // Actions
-        defaultBinding->ReqGo			= ::OIS::KeyCode::KC_N;
-        defaultBinding->Close			= ::OIS::KeyCode::KC_ESCAPE;
+      // Targetting
+      defaultBinding->NextTarget		= ::OIS::KeyCode::KC_TAB;
+      defaultBinding->SelfTarget		= ::OIS::KeyCode::KC_LMENU;
 
-        defaultBinding->ActionButton01 = ::OIS::KeyCode::KC_1;
-        defaultBinding->ActionButton02 = ::OIS::KeyCode::KC_2;
-        defaultBinding->ActionButton03 = ::OIS::KeyCode::KC_3;
-        defaultBinding->ActionButton04 = ::OIS::KeyCode::KC_4;
-        defaultBinding->ActionButton05 = ::OIS::KeyCode::KC_5;
-        defaultBinding->ActionButton06 = ::OIS::KeyCode::KC_6;
-        defaultBinding->ActionButton07 = ::OIS::KeyCode::KC_7;
-        defaultBinding->ActionButton08 = ::OIS::KeyCode::KC_8;
-        defaultBinding->ActionButton09 = ::OIS::KeyCode::KC_9;
-        defaultBinding->ActionButton10 = ::OIS::KeyCode::KC_0;
-        defaultBinding->ActionButton11 = ::OIS::KeyCode::KC_H;
-        defaultBinding->ActionButton12 = ::OIS::KeyCode::KC_H;
-        defaultBinding->ActionButton13 = ::OIS::KeyCode::KC_SPACE;
-        defaultBinding->ActionButton14 = ::OIS::KeyCode::KC_R;
-        defaultBinding->ActionButton15 = ::OIS::KeyCode::KC_G;
-        defaultBinding->ActionButton16 = ::OIS::KeyCode::KC_I;
-        defaultBinding->ActionButton17 = ::OIS::KeyCode::KC_U;
-        defaultBinding->ActionButton18 = ::OIS::KeyCode::KC_B;
-        defaultBinding->ActionButton19 = ::OIS::KeyCode::KC_T;
-        defaultBinding->ActionButton20 = ::OIS::KeyCode::KC_F;
-        defaultBinding->ActionButton21 = ::OIS::KeyCode::KC_C;
-        defaultBinding->ActionButton22 = ::OIS::KeyCode::KC_V;
-        defaultBinding->ActionButton23 = ::OIS::KeyCode::KC_J;
-        defaultBinding->ActionButton24 = ::OIS::KeyCode::KC_J;
-		defaultBinding->ActionButton25 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton26 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton27 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton28 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton29 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton30 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton31 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton32 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton33 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton34 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton35 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton36 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton37 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton38 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton39 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton40 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton41 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton42 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton43 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton44 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton45 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton46 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton47 = ::OIS::KeyCode::KC_UNASSIGNED;
-		defaultBinding->ActionButton48 = ::OIS::KeyCode::KC_UNASSIGNED;
+      // Actions
+      defaultBinding->ReqGo			= ::OIS::KeyCode::KC_SPACE;
+      defaultBinding->Close			= ::OIS::KeyCode::KC_ESCAPE;
+
+      defaultBinding->ActionButton01 = ::OIS::KeyCode::KC_Q;
+      defaultBinding->ActionButton02 = ::OIS::KeyCode::KC_E;
+      defaultBinding->ActionButton03 = ::OIS::KeyCode::KC_R;
+      defaultBinding->ActionButton04 = ::OIS::KeyCode::KC_F;
+      defaultBinding->ActionButton05 = ::OIS::KeyCode::KC_T;
+      defaultBinding->ActionButton06 = ::OIS::KeyCode::KC_G;
+      defaultBinding->ActionButton07 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton08 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton09 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton10 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton11 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton12 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton13 = ::OIS::KeyCode::KC_Y;
+      defaultBinding->ActionButton14 = ::OIS::KeyCode::KC_X;
+      defaultBinding->ActionButton15 = ::OIS::KeyCode::KC_C;
+      defaultBinding->ActionButton16 = ::OIS::KeyCode::KC_V;
+      defaultBinding->ActionButton17 = ::OIS::KeyCode::KC_B;
+      defaultBinding->ActionButton18 = ::OIS::KeyCode::KC_N;
+      defaultBinding->ActionButton19 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton20 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton21 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton22 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton23 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton24 = ::OIS::KeyCode::KC_UNASSIGNED;
+      defaultBinding->ActionButton25 = ::OIS::KeyCode::KC_F1;
+      defaultBinding->ActionButton26 = ::OIS::KeyCode::KC_F2;
+      defaultBinding->ActionButton27 = ::OIS::KeyCode::KC_F3;
+      defaultBinding->ActionButton28 = ::OIS::KeyCode::KC_F4;
+      defaultBinding->ActionButton29 = ::OIS::KeyCode::KC_F5;
+      defaultBinding->ActionButton30 = ::OIS::KeyCode::KC_F6;
+      defaultBinding->ActionButton31 = ::OIS::KeyCode::KC_F7;
+      defaultBinding->ActionButton32 = ::OIS::KeyCode::KC_F8;
+      defaultBinding->ActionButton33 = ::OIS::KeyCode::KC_F9;
+      defaultBinding->ActionButton34 = ::OIS::KeyCode::KC_F10;
+      defaultBinding->ActionButton35 = ::OIS::KeyCode::KC_F11;
+      defaultBinding->ActionButton36 = ::OIS::KeyCode::KC_F12;
+      defaultBinding->ActionButton37 = ::OIS::KeyCode::KC_1;
+      defaultBinding->ActionButton38 = ::OIS::KeyCode::KC_2;
+      defaultBinding->ActionButton39 = ::OIS::KeyCode::KC_3;
+      defaultBinding->ActionButton40 = ::OIS::KeyCode::KC_4;
+      defaultBinding->ActionButton41 = ::OIS::KeyCode::KC_5;
+      defaultBinding->ActionButton42 = ::OIS::KeyCode::KC_6;
+      defaultBinding->ActionButton43 = ::OIS::KeyCode::KC_7;
+      defaultBinding->ActionButton44 = ::OIS::KeyCode::KC_8;
+      defaultBinding->ActionButton45 = ::OIS::KeyCode::KC_9;
+      defaultBinding->ActionButton46 = ::OIS::KeyCode::KC_0;
+      defaultBinding->ActionButton47 = ::OIS::KeyCode::KC_GRAVE;
+      defaultBinding->ActionButton48 = ::OIS::KeyCode::KC_UNASSIGNED;
 
         return defaultBinding;      
     };	
