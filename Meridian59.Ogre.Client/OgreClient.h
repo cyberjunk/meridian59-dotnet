b357ee267917ab6aace0f31cdbe2ddd665e6639c
skittles1 <tmx883@gmail.com>
2016-12-20 03:47:54 +0800

    Increment Ogre version.
    Incremented Ogre client minor version/milestone? version as we are about
    to push the latest updates to users.

--- Meridian59.Ogre.Client/OgreClient.h
+++ Meridian59.Ogre.Client/OgreClient.h
@@ -248,7 +248,7 @@ namespace Meridian59 { namespace Ogre
 
 		property unsigned char AppVersionMinor
 		{ 
-			public: virtual unsigned char get() override { return 10; } 			
+			public: virtual unsigned char get() override { return 11; } 			
 		};
 		
 		property ::Ogre::Root* Root 
