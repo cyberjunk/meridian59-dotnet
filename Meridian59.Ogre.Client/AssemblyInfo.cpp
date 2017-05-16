620ec1f846967b34b71e6910d5fab78bb06a9849
clint.banzhaf <clint.banzhaf@CLINT>
2017-05-16 11:49:15 +0200

    Increment OgreClient version to 1.0.4.8

--- Meridian59.Ogre.Client/AssemblyInfo.cpp
+++ Meridian59.Ogre.Client/AssemblyInfo.cpp
@@ -14,6 +14,6 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyCopyrightAttribute("GPL 3.0")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
-[assembly:AssemblyVersionAttribute("1.0.4.7")];
+[assembly:AssemblyVersionAttribute("1.0.4.8")];
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
