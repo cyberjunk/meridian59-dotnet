a5e570cc5694e563f7bd24e293a1b817db2948e1
clint.banzhaf <clint.banzhaf@CLINT>
2017-05-25 18:20:34 +0200

    Cleanup UIChat.cpp

--- Meridian59.Ogre.Client/UIChat.cpp
+++ Meridian59.Ogre.Client/UIChat.cpp
@@ -2,49 +2,49 @@
 
 namespace Meridian59 { namespace Ogre
 {
-	void ControllerUI::Chat::Initialize()
-	{
-		// setup references to children from xml nodes
-		Window		= static_cast<CEGUI::FrameWindow*>(guiRoot->getChild(UI_NAME_CHAT_WINDOW));
-		Text		= static_cast<CEGUI::Window*>(Window->getChild(UI_NAME_CHAT_TEXT));
-      TextPlain = static_cast<CEGUI::MultiLineEditbox*>(Window->getChild(UI_NAME_CHAT_TEXTPLAIN));
-      Input		= static_cast<CEGUI::Editbox*>(Window->getChild(UI_NAME_CHAT_INPUT));
-		Scrollbar	= static_cast<CEGUI::Scrollbar*>(Text->getChildAtIdx(1));
+   void ControllerUI::Chat::Initialize()
+   {
+      // setup references to children from xml nodes
+      Window         = static_cast<CEGUI::FrameWindow*>(guiRoot->getChild(UI_NAME_CHAT_WINDOW));
+      Text           = static_cast<CEGUI::Window*>(Window->getChild(UI_NAME_CHAT_TEXT));
+      TextPlain      = static_cast<CEGUI::MultiLineEditbox*>(Window->getChild(UI_NAME_CHAT_TEXTPLAIN));
+      Input          = static_cast<CEGUI::Editbox*>(Window->getChild(UI_NAME_CHAT_INPUT));
+      Scrollbar      = static_cast<CEGUI::Scrollbar*>(Text->getChildAtIdx(1));
       ScrollbarPlain = TextPlain->getVertScrollbar();
 
-		// set window layout from config
-		Window->setPosition(OgreClient::Singleton->Config->UILayoutChat->getPosition());
-		Window->setSize(OgreClient::Singleton->Config->UILayoutChat->getSize());
+      // set window layout from config
+      Window->setPosition(OgreClient::Singleton->Config->UILayoutChat->getPosition());
+      Window->setSize(OgreClient::Singleton->Config->UILayoutChat->getSize());
 
-		// set autoscroll on text at start
-		Scrollbar->setEndLockEnabled(true);
+      // set autoscroll on text at start
+      Scrollbar->setEndLockEnabled(true);
       ScrollbarPlain->setEndLockEnabled(true);
 
-		// set maximum textlength for chatinput
-		Input->setMaxTextLength((size_t)BlakservStringLengths::MAX_CHAT_LEN);
+      // set maximum textlength for chatinput
+      Input->setMaxTextLength((size_t)BlakservStringLengths::MAX_CHAT_LEN);
 
       TextPlain->setVerticalAlignment(::CEGUI::VerticalAlignment::VA_TOP);
       TextPlain->setHorizontalAlignment(::CEGUI::HorizontalAlignment::HA_LEFT);
       TextPlain->setEnsureCaretVisible(false);
 
-		// attach listener to chatmessage list
-		OgreClient::Singleton->Data->ChatMessages->ListChanged += 
-			gcnew ListChangedEventHandler(&ControllerUI::Chat::OnChatMessagesListChanged);
+      // attach listener to chatmessage list
+      OgreClient::Singleton->Data->ChatMessages->ListChanged += 
+         gcnew ListChangedEventHandler(&ControllerUI::Chat::OnChatMessagesListChanged);
             
-		// subscribe key-up event
-		Input->subscribeEvent(CEGUI::Editbox::EventKeyDown, CEGUI::Event::Subscriber(UICallbacks::Chat::OnKeyDown));
-		
-		// subscribe scroll
-		Scrollbar->subscribeEvent(CEGUI::Scrollbar::EventThumbTrackStarted, CEGUI::Event::Subscriber(UICallbacks::Chat::OnThumbTrackStarted));
-		Scrollbar->subscribeEvent(CEGUI::Scrollbar::EventThumbTrackEnded, CEGUI::Event::Subscriber(UICallbacks::Chat::OnThumbTrackEnded));
+      // subscribe key-up event
+      Input->subscribeEvent(CEGUI::Editbox::EventKeyDown, CEGUI::Event::Subscriber(UICallbacks::Chat::OnKeyDown));
+
+      // subscribe scroll
+      Scrollbar->subscribeEvent(CEGUI::Scrollbar::EventThumbTrackStarted, CEGUI::Event::Subscriber(UICallbacks::Chat::OnThumbTrackStarted));
+      Scrollbar->subscribeEvent(CEGUI::Scrollbar::EventThumbTrackEnded, CEGUI::Event::Subscriber(UICallbacks::Chat::OnThumbTrackEnded));
       ScrollbarPlain->subscribeEvent(CEGUI::Scrollbar::EventThumbTrackStarted, CEGUI::Event::Subscriber(UICallbacks::Chat::OnThumbTrackStarted));
       ScrollbarPlain->subscribeEvent(CEGUI::Scrollbar::EventThumbTrackEnded, CEGUI::Event::Subscriber(UICallbacks::Chat::OnThumbTrackEnded));
 
-		// subscribe close button
-		Window->subscribeEvent(CEGUI::FrameWindow::EventCloseClicked, CEGUI::Event::Subscriber(UICallbacks::OnWindowClosed));
+      // subscribe close button
+      Window->subscribeEvent(CEGUI::FrameWindow::EventCloseClicked, CEGUI::Event::Subscriber(UICallbacks::OnWindowClosed));
 
-		// subscribe keyup
-		Window->subscribeEvent(CEGUI::FrameWindow::EventKeyUp, CEGUI::Event::Subscriber(UICallbacks::OnKeyUp));
+      // subscribe keyup
+      Window->subscribeEvent(CEGUI::FrameWindow::EventKeyUp, CEGUI::Event::Subscriber(UICallbacks::OnKeyUp));
 
       // subscribe click
       Text->subscribeEvent(CEGUI::Window::EventMouseClick, CEGUI::Event::Subscriber(UICallbacks::Chat::OnTextClicked));
@@ -53,27 +53,27 @@ namespace Meridian59 { namespace Ogre
       // subscripe copy paste handler on plain chattext
       TextPlain->subscribeEvent(CEGUI::Window::EventKeyDown, CEGUI::Event::Subscriber(UICallbacks::OnCopyPasteKeyDown));
 
-		// create queue for chatmessage to write at next chatupdate tick
-		Queue = gcnew ::System::Collections::Generic::Queue<::Meridian59::Data::Models::ServerString^>();
-	};
+      // create queue for chatmessage to write at next chatupdate tick
+      Queue = gcnew ::System::Collections::Generic::Queue<::Meridian59::Data::Models::ServerString^>();
+   };
 
-	void ControllerUI::Chat::Destroy()
-	{	
-		OgreClient::Singleton->Data->ChatMessages->ListChanged -= 
-			gcnew ListChangedEventHandler(&ControllerUI::Chat::OnChatMessagesListChanged); 
+   void ControllerUI::Chat::Destroy()
+   {	
+      OgreClient::Singleton->Data->ChatMessages->ListChanged -= 
+         gcnew ListChangedEventHandler(&ControllerUI::Chat::OnChatMessagesListChanged); 
 
-		// reset delete counter
-		DeleteCounter = 0;
-		
-		// cleanup queue
-		Queue->Clear();	
-		delete Queue;
-		Queue = nullptr;
-	};
+      // reset delete counter
+      DeleteCounter = 0;
 
-	void ControllerUI::Chat::ApplyLanguage()
-	{
-	};
+      // cleanup queue
+      Queue->Clear();	
+      delete Queue;
+      Queue = nullptr;
+   };
+
+   void ControllerUI::Chat::ApplyLanguage()
+   {
+   };
 
    void ControllerUI::Chat::Tick(double Tick, double Span)
    {
@@ -175,125 +175,76 @@ namespace Meridian59 { namespace Ogre
       ChatForceRenew = false;
    };
 
-	::CEGUI::String ControllerUI::Chat::GetChatString(ServerString^ ChatMessage)
-	{
+   ::CEGUI::String ControllerUI::Chat::GetChatString(ServerString^ ChatMessage)
+   {
       if (PlainMode)
          return StringConvert::CLRToCEGUI(ChatMessage->FullString + ::System::Environment::NewLine);
 
-		// text with CEGUI markup and escapes
-		::System::String^ text = ::System::String::Empty;
+      // text with CEGUI markup and escapes
+      ::System::String^ text = ::System::String::Empty;
 
-		// walk style definitions
-		for each (ChatStyle^ style in ChatMessage->Styles)
-		{
-			// add fontselection
-			// normal
-			if (!style->IsBold && !style->IsCursive)			
-				text += UI_TAGFONT_NORMAL;
-			
-			// bold not italic
-			else if (style->IsBold && !style->IsCursive)			
-				text += UI_TAGFONT_BOLD;
-				
-			// not bold but italic
-			else if (!style->IsBold && style->IsCursive)			
-				text += UI_TAGFONT_ITALIC;
+      // walk style definitions
+      for each (ChatStyle^ style in ChatMessage->Styles)
+      {
+         // add fontselection
+         // normal
+         if (!style->IsBold && !style->IsCursive)
+            text += UI_TAGFONT_NORMAL;
 
-			// bold and italic
-			else if (style->IsBold && style->IsCursive)			
-				text += UI_TAGFONT_BOLDITALIC;				
-			
-			// add color
-			switch(style->Color)
-			{
-				case ChatColor::Black:
-					text += UI_TAGCOLOR_BLACK;
-					break;
+         // bold not italic
+         else if (style->IsBold && !style->IsCursive)
+            text += UI_TAGFONT_BOLD;
 
-				case ChatColor::Blue:
-					text += UI_TAGCOLOR_BLUE;
-					break;
+         // not bold but italic
+         else if (!style->IsBold && style->IsCursive)
+            text += UI_TAGFONT_ITALIC;
 
-				case ChatColor::Green:
-					text += UI_TAGCOLOR_CHATGREEN;
-					break;
+         // bold and italic
+         else if (style->IsBold && style->IsCursive)
+            text += UI_TAGFONT_BOLDITALIC;
 
-				case ChatColor::Purple:
-					text += UI_TAGCOLOR_CHATPURPLE;
-					break;
-				
-				case ChatColor::Red:
-					text += UI_TAGCOLOR_CHATRED;
-					break;
-
-				case ChatColor::White:
-					text += UI_TAGCOLOR_WHITE;
-					break;
+         // add color
+         switch(style->Color)
+         {
+            case ChatColor::Black: text += UI_TAGCOLOR_BLACK; break;
+            case ChatColor::Blue: text += UI_TAGCOLOR_BLUE; break;
+            case ChatColor::Green: text += UI_TAGCOLOR_CHATGREEN; break;
+            case ChatColor::Purple: text += UI_TAGCOLOR_CHATPURPLE; break;
+            case ChatColor::Red: text += UI_TAGCOLOR_CHATRED; break;
+            case ChatColor::White: text += UI_TAGCOLOR_WHITE; break;
 #ifndef VANILLA
-				case ChatColor::BrightRed:
-					text += UI_TAGCOLOR_CHATBRIGHTRED;
-					break;
-
-				case ChatColor::LightGreen:
-					text += UI_TAGCOLOR_CHATLIGHTGREEN;
-					break;
-
-				case ChatColor::Yellow:
-					text += UI_TAGCOLOR_CHATYELLOW;
-					break;
-
-				case ChatColor::Pink:
-					text += UI_TAGCOLOR_CHATPINK;
-					break;
-
-				case ChatColor::Orange:
-					text += UI_TAGCOLOR_CHATORANGE;
-					break;
-
-				case ChatColor::Aquamarine:
-					text += UI_TAGCOLOR_CHATAQUAMARINE;
-					break;
-
-				case ChatColor::Cyan:
-					text += UI_TAGCOLOR_CHATCYAN;
-					break;
-
-				case ChatColor::Teal:
-					text += UI_TAGCOLOR_CHATTEAL;
-					break;
-
-				case ChatColor::DarkGrey:
-					text += UI_TAGCOLOR_CHATDARKGREY;
-					break;
-
-				case ChatColor::Violet:
-					text += UI_TAGCOLOR_CHATVIOLET;
-					break;
-
-				case ChatColor::Magenta:
-					text += UI_TAGCOLOR_CHATMAGENTA;
-					break;
+            case ChatColor::BrightRed: text += UI_TAGCOLOR_CHATBRIGHTRED; break;
+            case ChatColor::LightGreen: text += UI_TAGCOLOR_CHATLIGHTGREEN; break;
+            case ChatColor::Yellow: text += UI_TAGCOLOR_CHATYELLOW; break;
+            case ChatColor::Pink: text += UI_TAGCOLOR_CHATPINK; break;
+            case ChatColor::Orange: text += UI_TAGCOLOR_CHATORANGE; break;
+            case ChatColor::Aquamarine: text += UI_TAGCOLOR_CHATAQUAMARINE; break;
+            case ChatColor::Cyan: text += UI_TAGCOLOR_CHATCYAN; break;
+            case ChatColor::Teal: text += UI_TAGCOLOR_CHATTEAL; break;
+            case ChatColor::DarkGrey: text += UI_TAGCOLOR_CHATDARKGREY; break;
+            case ChatColor::Violet: text += UI_TAGCOLOR_CHATVIOLET; break;
+            case ChatColor::Magenta: text += UI_TAGCOLOR_CHATMAGENTA; break;
 #endif
-			}
+         }
 
-			// get substring for this style
-			::System::String^ str = ChatMessage->FullString->Substring(style->StartIndex, style->Length);
+         // get substring for this style
+         ::System::String^ str = ChatMessage->FullString->Substring(style->StartIndex, style->Length);
 
-			// replace "\" with "\\"	
-			// and "[" with "\["
-			str = str->Replace("\\", "\\\\");
-			str = str->Replace("[", "\\[");
+         // replace "\" with "\\"
+         // and "[" with "\["
+         str = str->Replace("\\", "\\\\");
+         str = str->Replace("[", "\\[");
 
-			// add textpart
-			text += str;		
-		}
+         // add textpart
+         text += str;
+      }
 
-		// append line break
-		text += ::System::Environment::NewLine;
+      // append line break
+      text += ::System::Environment::NewLine;
 
-		// return as CEGUI string
-		return StringConvert::CLRToCEGUI(text);
-	};
+      // return as CEGUI string
+      return StringConvert::CLRToCEGUI(text);
+   };
 
    void ControllerUI::Chat::OnChatMessagesListChanged(Object^ sender, ListChangedEventArgs^ e)
    {
@@ -308,89 +259,77 @@ namespace Meridian59 { namespace Ogre
             break;
 
          case ::System::ComponentModel::ListChangedType::ItemDeleted:
-            DeleteCounter++;		
+            DeleteCounter++;
             break;
       }
    };
 
-	bool UICallbacks::Chat::OnKeyDown(const CEGUI::EventArgs& e)
-	{
-		const CEGUI::KeyEventArgs& e2 = static_cast<const CEGUI::KeyEventArgs&>(e);
-		CEGUI::KeyEventArgs& args	= const_cast<CEGUI::KeyEventArgs&>(e2);
-		CEGUI::Editbox* chatInput	= ControllerUI::Chat::Input;
-		const CEGUI::String text	= chatInput->getText();				
-		
-		::System::Collections::Generic::List<::System::String^>^ chatCommandHistory =
-			OgreClient::Singleton->Data->ChatCommandHistory;
+   bool UICallbacks::Chat::OnKeyDown(const CEGUI::EventArgs& e)
+   {
+      const CEGUI::KeyEventArgs& e2 = static_cast<const CEGUI::KeyEventArgs&>(e);
+      CEGUI::KeyEventArgs& args     = const_cast<CEGUI::KeyEventArgs&>(e2);
+      CEGUI::Editbox* chatInput     = ControllerUI::Chat::Input;
+      const CEGUI::String& text     = chatInput->getText();
 
-		// base handler for copy&paste clipboard
-		bool handled = UICallbacks::OnCopyPasteKeyDown(e);	
-		::System::String^ str;
+      ::System::Collections::Generic::List<::System::String^>^ chatCommandHistory =
+         OgreClient::Singleton->Data->ChatCommandHistory;
 
-		switch(args.scancode)
-		{
-			case CEGUI::Key::Scan::Return:
-			case CEGUI::Key::Scan::NumpadEnter:
-				// exec chatcommand
-				OgreClient::Singleton->ExecChatCommand(
-					StringConvert::CEGUIToCLR(text));
+      // base handler for copy&paste clipboard
+      bool handled = UICallbacks::OnCopyPasteKeyDown(e);
 
-				// clear input
-				chatInput->setText(STRINGEMPTY);
-				
-				// reset chatcommand history scrollindex
-				OgreClient::Singleton->Data->ChatCommandHistoryIndex = -1;
+      // used in some cases
+      ::System::String^ str;
 
-				// focus back game
-				ControllerUI::ActivateRoot();
-							
-				handled = true;
-				break;
-
-			case CEGUI::Key::Scan::Escape:
-				// reset chatcommand history scrollindex
-				OgreClient::Singleton->Data->ChatCommandHistoryIndex = -1;
-
-				ControllerUI::ActivateRoot();
-				handled = true;
-				break;
-
-			case CEGUI::Key::ArrowUp:
-				str = OgreClient::Singleton->Data->ChatCommandHistoryGetNext();
-
-				if (str)
-				{
-					::CEGUI::String ceguistr = StringConvert::CLRToCEGUI(str);
-
-					chatInput->setText(ceguistr);
-					chatInput->setCaretIndex(ceguistr.length());
-				}
+      switch(args.scancode)
+      {
+         case CEGUI::Key::Scan::Return:
+         case CEGUI::Key::Scan::NumpadEnter:
+            // exec chatcommand
+            OgreClient::Singleton->ExecChatCommand(StringConvert::CEGUIToCLR(text));
+            chatInput->setText(STRINGEMPTY);
+            OgreClient::Singleton->Data->ChatCommandHistoryIndex = -1;
+            ControllerUI::ActivateRoot();
             handled = true;
-				break;
+            break;
 
-			case CEGUI::Key::ArrowDown:
-				str = OgreClient::Singleton->Data->ChatCommandHistoryGetPrevious();
-
-				if (str)
-				{
-					::CEGUI::String ceguistr = StringConvert::CLRToCEGUI(str);
-
-					chatInput->setText(ceguistr);
-					chatInput->setCaretIndex(ceguistr.length());
-				}
-				else
-				{
-					chatInput->setText(STRINGEMPTY);
-				}
+         case CEGUI::Key::Scan::Escape:
+            OgreClient::Singleton->Data->ChatCommandHistoryIndex = -1;
+            ControllerUI::ActivateRoot();
             handled = true;
-				break;
-		}	
+            break;
 
-		if (handled)
-			args.handled++;
+         case CEGUI::Key::ArrowUp:
+            str = OgreClient::Singleton->Data->ChatCommandHistoryGetNext();
+            if (str)
+            {
+               ::CEGUI::String& ceguistr = StringConvert::CLRToCEGUI(str);
+               chatInput->setText(ceguistr);
+               chatInput->setCaretIndex(ceguistr.length());
+            }
+            handled = true;
+            break;
 
-		return handled;
-	};
+         case CEGUI::Key::ArrowDown:
+            str = OgreClient::Singleton->Data->ChatCommandHistoryGetPrevious();
+            if (str)
+            {
+               ::CEGUI::String& ceguistr = StringConvert::CLRToCEGUI(str);
+               chatInput->setText(ceguistr);
+               chatInput->setCaretIndex(ceguistr.length());
+            }
+            else
+            {
+               chatInput->setText(STRINGEMPTY);
+            }
+            handled = true;
+            break;
+      }
+
+      if (handled)
+         args.handled++;
+
+      return handled;
+   };
 
    bool UICallbacks::Chat::OnThumbTrackStarted(const CEGUI::EventArgs& e)
    {
