105f4b6657580294345f91ec43ba0ed2838e2ea6
clint.banzhaf <clint.banzhaf@CLINT>
2017-05-31 23:15:44 +0200

    Formatting UIActions.cpp

--- Meridian59.Ogre.Client/UIActions.cpp
+++ Meridian59.Ogre.Client/UIActions.cpp
@@ -2,169 +2,140 @@
 
 namespace Meridian59 { namespace Ogre
 {
-	void ControllerUI::Actions::Initialize()
-	{
-		// setup references to children from xml nodes
-		Window	= static_cast<CEGUI::FrameWindow*>(guiRoot->getChild(UI_NAME_ACTIONS_WINDOW));
-		List	= static_cast<CEGUI::ItemListbox*>(Window->getChild(UI_NAME_ACTIONS_LIST));
-		
-		// set window layout from config
-		Window->setPosition(OgreClient::Singleton->Config->UILayoutActions->getPosition());
-		Window->setSize(OgreClient::Singleton->Config->UILayoutActions->getSize());
+   void ControllerUI::Actions::Initialize()
+   {
+      // setup references to children from xml nodes
+      Window = static_cast<CEGUI::FrameWindow*>(guiRoot->getChild(UI_NAME_ACTIONS_WINDOW));
+      List   = static_cast<CEGUI::ItemListbox*>(Window->getChild(UI_NAME_ACTIONS_LIST));
 
-		// create action entries
-		CreateItem(AvatarAction::Attack);
-		CreateItem(AvatarAction::Rest);
-		CreateItem(AvatarAction::Dance);
-		CreateItem(AvatarAction::Wave);
-		CreateItem(AvatarAction::Point);
-		CreateItem(AvatarAction::Loot);
-		CreateItem(AvatarAction::Buy);
-		CreateItem(AvatarAction::Inspect);
-		CreateItem(AvatarAction::Trade);
-		CreateItem(AvatarAction::Activate);
-		CreateItem(AvatarAction::GuildInvite);
+      // set window layout from config
+      Window->setPosition(OgreClient::Singleton->Config->UILayoutActions->getPosition());
+      Window->setSize(OgreClient::Singleton->Config->UILayoutActions->getSize());
 
-		// subscribe close button
-		Window->subscribeEvent(CEGUI::FrameWindow::EventCloseClicked, CEGUI::Event::Subscriber(UICallbacks::OnWindowClosed));
+      // create action entries
+      CreateItem(AvatarAction::Attack);
+      CreateItem(AvatarAction::Rest);
+      CreateItem(AvatarAction::Dance);
+      CreateItem(AvatarAction::Wave);
+      CreateItem(AvatarAction::Point);
+      CreateItem(AvatarAction::Loot);
+      CreateItem(AvatarAction::Buy);
+      CreateItem(AvatarAction::Inspect);
+      CreateItem(AvatarAction::Trade);
+      CreateItem(AvatarAction::Activate);
+      CreateItem(AvatarAction::GuildInvite);
 
-		// subscribe keyup
-		Window->subscribeEvent(CEGUI::FrameWindow::EventKeyUp, CEGUI::Event::Subscriber(UICallbacks::OnKeyUp));
-	};
+      // subscribe close button
+      Window->subscribeEvent(CEGUI::FrameWindow::EventCloseClicked, CEGUI::Event::Subscriber(UICallbacks::OnWindowClosed));
 
-	void ControllerUI::Actions::Destroy()
-	{				
-	};
+      // subscribe keyup
+      Window->subscribeEvent(CEGUI::FrameWindow::EventKeyUp, CEGUI::Event::Subscriber(UICallbacks::OnKeyUp));
+   };
 
-	void ControllerUI::Actions::ApplyLanguage()
-	{
-		Window->setText(GetLangWindowTitle(LANGSTR_WINDOW_TITLE::ACTIONS));
-	};
+   void ControllerUI::Actions::Destroy()
+   {
+   };
 
-	void ControllerUI::Actions::CreateItem(AvatarAction Type)
-	{
-		CEGUI::WindowManager* wndMgr = CEGUI::WindowManager::getSingletonPtr();
+   void ControllerUI::Actions::ApplyLanguage()
+   {
+      Window->setText(GetLangWindowTitle(LANGSTR_WINDOW_TITLE::ACTIONS));
+   };
 
-		// create widget (item)
-		CEGUI::ItemEntry* widget = (CEGUI::ItemEntry*)wndMgr->createWindow(
-			UI_WINDOWTYPE_ACTIONITEM);
+   void ControllerUI::Actions::CreateItem(AvatarAction Type)
+   {
+      CEGUI::WindowManager* wndMgr = CEGUI::WindowManager::getSingletonPtr();
 
-		// save avataraction enum value in ID
-		widget->setID((unsigned int)Type);
+      // create widget (item)
+      CEGUI::ItemEntry* widget = (CEGUI::ItemEntry*)wndMgr->createWindow(
+         UI_WINDOWTYPE_ACTIONITEM);
 
-		// subscribe doubleclick event
-		widget->subscribeEvent(
-			CEGUI::ItemEntry::EventMouseDoubleClick, 
-			CEGUI::Event::Subscriber(UICallbacks::Actions::OnItemDoubleClicked));
-		
-		// check
-		if (widget->getChildCount() > 1)
-		{
-			CEGUI::DragContainer* iconDrag = (CEGUI::DragContainer*)widget->getChildAtIdx(UI_ACTIONS_CHILDINDEX_ICON);
-			CEGUI::Window* icon	= iconDrag->getChildAtIdx(0);			
-			CEGUI::Window* name	= (CEGUI::Window*)widget->getChildAtIdx(UI_ACTIONS_CHILDINDEX_NAME);
+      // save avataraction enum value in ID
+      widget->setID((unsigned int)Type);
 
-			// subscribe drag start and end to draggable icon
-			iconDrag->subscribeEvent(CEGUI::DragContainer::EventDragStarted, CEGUI::Event::Subscriber(UICallbacks::Actions::OnDragStarted));
-			iconDrag->subscribeEvent(CEGUI::DragContainer::EventDragEnded, CEGUI::Event::Subscriber(UICallbacks::Actions::OnDragEnded));
+      // subscribe doubleclick event
+      widget->subscribeEvent(
+         CEGUI::ItemEntry::EventMouseDoubleClick, 
+         CEGUI::Event::Subscriber(UICallbacks::Actions::OnItemDoubleClicked));
 
-			CEGUI::String strIconName;
+      // check
+      if (widget->getChildCount() > 1)
+      {
+         CEGUI::DragContainer* iconDrag = (CEGUI::DragContainer*)widget->getChildAtIdx(UI_ACTIONS_CHILDINDEX_ICON);
+         CEGUI::Window* icon            = iconDrag->getChildAtIdx(0);
+         CEGUI::Window* name            = (CEGUI::Window*)widget->getChildAtIdx(UI_ACTIONS_CHILDINDEX_NAME);
 
-			// note: this can not be translated yet, because it must match the enum value
-			CEGUI::String strName = StringConvert::CLRToCEGUI(Type.ToString());
+         // subscribe drag start and end to draggable icon
+         iconDrag->subscribeEvent(CEGUI::DragContainer::EventDragStarted, CEGUI::Event::Subscriber(UICallbacks::Actions::OnDragStarted));
+         iconDrag->subscribeEvent(CEGUI::DragContainer::EventDragEnded, CEGUI::Event::Subscriber(UICallbacks::Actions::OnDragEnded));
 
-			switch(Type)
-			{
-				case AvatarAction::Attack:
-					strIconName = UI_IMAGE_ACTION_ATTACK;
-					break;
+         CEGUI::String strIconName;
 
-				case AvatarAction::Rest:
-					strIconName = UI_IMAGE_ACTION_REST;
-					break;
+         // note: this can not be translated yet, because it must match the enum value
+         CEGUI::String strName = StringConvert::CLRToCEGUI(Type.ToString());
 
-				case AvatarAction::Dance:
-					strIconName = UI_IMAGE_ACTION_DANCE;
-					break;
+         switch(Type)
+         {
+         case AvatarAction::Attack:       strIconName = UI_IMAGE_ACTION_ATTACK;        break;
+         case AvatarAction::Rest:         strIconName = UI_IMAGE_ACTION_REST;          break;
+         case AvatarAction::Dance:        strIconName = UI_IMAGE_ACTION_DANCE;         break;
+         case AvatarAction::Wave:         strIconName = UI_IMAGE_ACTION_WAVE;          break;
+         case AvatarAction::Point:        strIconName = UI_IMAGE_ACTION_POINT;         break;
+         case AvatarAction::Loot:         strIconName = UI_IMAGE_ACTION_LOOT;          break;
+         case AvatarAction::Buy:          strIconName = UI_IMAGE_ACTION_BUY;           break;
+         case AvatarAction::Inspect:      strIconName = UI_IMAGE_ACTION_INSPECT;       break;
+         case AvatarAction::Trade:        strIconName = UI_IMAGE_ACTION_TRADE;         break;
+         case AvatarAction::Activate:     strIconName = UI_IMAGE_ACTION_ACTIVATE;      break;
+         case AvatarAction::GuildInvite:  strIconName = UI_IMAGE_ACTION_GUILDINVITE;   break;
 
-				case AvatarAction::Wave:
-					strIconName = UI_IMAGE_ACTION_WAVE;
-					break;
+         default:
+            strIconName = STRINGEMPTY;
+            strName = STRINGEMPTY;
+            break;
+         }
 
-				case AvatarAction::Point:
-					strIconName = UI_IMAGE_ACTION_POINT;
-					break;
+         // set name
+         name->setText(strName);
 
-				case AvatarAction::Loot:
-					strIconName = UI_IMAGE_ACTION_LOOT;
-					break;
+         if (strIconName != STRINGEMPTY)
+            icon->setProperty(UI_PROPNAME_IMAGE, strIconName);
+      }
 
-				case AvatarAction::Buy:
-					strIconName = UI_IMAGE_ACTION_BUY;
-					break;
+      // add
+      List->addItem(widget);
 
-				case AvatarAction::Inspect:
-					strIconName = UI_IMAGE_ACTION_INSPECT;
-					break;
+      // fix a big with last item not selectable
+      // when insertItem was used
+      List->notifyScreenAreaChanged(true);
+   };
 
-				case AvatarAction::Trade:
-					strIconName = UI_IMAGE_ACTION_TRADE;
-					break;
+   //////////////////////////////////////////////////////////////////////////////////////////////////////////
+   //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-				case AvatarAction::Activate:
-					strIconName = UI_IMAGE_ACTION_ACTIVATE;
-					break;
+   bool UICallbacks::Actions::OnItemDoubleClicked(const CEGUI::EventArgs& e)
+   {
+      const CEGUI::MouseEventArgs& args = static_cast<const CEGUI::MouseEventArgs&>(e);
+      const CEGUI::ItemEntry* itm = (CEGUI::ItemEntry*)args.window;
 
-				case AvatarAction::GuildInvite:
-					strIconName = UI_IMAGE_ACTION_GUILDINVITE;
-					break;
+      // get avataraction enum value set in ID
+      AvatarAction action = (AvatarAction)itm->getID();
 
-				default:
-					strIconName = STRINGEMPTY;
-					strName = STRINGEMPTY;
-					break;
-			}
+      // execute action
+      OgreClient::Singleton->ExecAction(action);
 
-			// set name
-			name->setText(strName);
+      return true;
+   };
 
-			if (strIconName != STRINGEMPTY)
-				icon->setProperty(UI_PROPNAME_IMAGE, strIconName);
-		}
+   bool UICallbacks::Actions::OnDragStarted(const CEGUI::EventArgs& e)
+   {
+      const CEGUI::WindowEventArgs& args = static_cast<const CEGUI::WindowEventArgs&>(e);
+      ControllerUI::Actions::Window->setUsingAutoRenderingSurface(false);
+      return true;
+   };
 
-		// add
-		List->addItem(widget);
-
-		// fix a big with last item not selectable
-		// when insertItem was used
-		List->notifyScreenAreaChanged(true);
-	};
-	
-	bool UICallbacks::Actions::OnItemDoubleClicked(const CEGUI::EventArgs& e)
-	{
-		const CEGUI::MouseEventArgs& args = static_cast<const CEGUI::MouseEventArgs&>(e);
-		const CEGUI::ItemEntry* itm = (CEGUI::ItemEntry*)args.window;
-		
-		// get avataraction enum value set in ID
-		AvatarAction action = (AvatarAction)itm->getID();
-
-		// execute action
-		OgreClient::Singleton->ExecAction(action);
-
-		return true;
-	};
-
-	bool UICallbacks::Actions::OnDragStarted(const CEGUI::EventArgs& e)
-	{
-		const CEGUI::WindowEventArgs& args = static_cast<const CEGUI::WindowEventArgs&>(e);
-		ControllerUI::Actions::Window->setUsingAutoRenderingSurface(false);
-		return true;
-	};
-
-	bool UICallbacks::Actions::OnDragEnded(const CEGUI::EventArgs& e)
-	{
-		const CEGUI::WindowEventArgs& args = static_cast<const CEGUI::WindowEventArgs&>(e);
-		ControllerUI::Actions::Window->setUsingAutoRenderingSurface(true);
-		return true;
-	};
+   bool UICallbacks::Actions::OnDragEnded(const CEGUI::EventArgs& e)
+   {
+      const CEGUI::WindowEventArgs& args = static_cast<const CEGUI::WindowEventArgs&>(e);
+      ControllerUI::Actions::Window->setUsingAutoRenderingSurface(true);
+      return true;
+   };
 };};
