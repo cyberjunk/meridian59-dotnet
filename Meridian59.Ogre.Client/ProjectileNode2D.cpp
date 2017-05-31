c7615f4619f25b3d6d47d17171e58036e3e4e61d
clint.banzhaf <clint.banzhaf@CLINT>
2017-05-31 20:57:01 +0200

    More formatting

--- Meridian59.Ogre.Client/ProjectileNode2D.cpp
+++ Meridian59.Ogre.Client/ProjectileNode2D.cpp
@@ -2,166 +2,165 @@
 
 namespace Meridian59 { namespace Ogre 
 {
-	static ProjectileNode2D::ProjectileNode2D()
-	{
-	};
+   static ProjectileNode2D::ProjectileNode2D()
+   {
+   };
 
-	ProjectileNode2D::ProjectileNode2D(Data::Models::Projectile^ Projectile, ::Ogre::SceneManager* SceneManager)
-	{
-		this->Projectile = Projectile;
-        sceneManager = SceneManager;
-		hash = gcnew Murmur3(0);
+   ProjectileNode2D::ProjectileNode2D(Data::Models::Projectile^ Projectile, ::Ogre::SceneManager* SceneManager)
+   {
+      this->Projectile = Projectile;
+      sceneManager     = SceneManager;
+      hash             = gcnew Murmur3(0);
 
-		Projectile->PropertyChanged += 
-			gcnew PropertyChangedEventHandler(this, &ProjectileNode2D::OnProjectilePropertyChanged);
+      Projectile->PropertyChanged += 
+         gcnew PropertyChangedEventHandler(this, &ProjectileNode2D::OnProjectilePropertyChanged);
 
-		Projectile->AppearanceChanged += 
-			gcnew System::EventHandler(this, &ProjectileNode2D::OnProjectileAppearanceChanged);
+      Projectile->AppearanceChanged += 
+         gcnew System::EventHandler(this, &ProjectileNode2D::OnProjectileAppearanceChanged);
 
-		::Ogre::String& ostr_id		  = ::Ogre::StringConverter::toString(Projectile->ID);
-		::Ogre::String& ostr_billboard = PREFIX_PROJECTILE2D_BILLBOARD + ostr_id;
-		::Ogre::String& ostr_node	  = PREFIX_PROJECTILE2D_SCENENODE + ostr_id;
+      ::Ogre::String& ostr_id        = ::Ogre::StringConverter::toString(Projectile->ID);
+      ::Ogre::String& ostr_billboard = PREFIX_PROJECTILE2D_BILLBOARD + ostr_id;
+      ::Ogre::String& ostr_node      = PREFIX_PROJECTILE2D_SCENENODE + ostr_id;
 
-        // create billboardset for 1 billboard
-        billboardSet = sceneManager->createBillboardSet(ostr_billboard, 1);
-        billboardSet->setBillboardOrigin(BillboardOrigin::BBO_BOTTOM_CENTER);
-        billboardSet->setBillboardType(BillboardType::BBT_POINT);
-		billboardSet->setAutoextend(false);
+      // create billboardset for 1 billboard
+      billboardSet = sceneManager->createBillboardSet(ostr_billboard, 1);
+      billboardSet->setBillboardOrigin(BillboardOrigin::BBO_BOTTOM_CENTER);
+      billboardSet->setBillboardType(BillboardType::BBT_POINT);
+      billboardSet->setAutoextend(false);
 
-		// hide billboardset by default with no boundingbox (blank objects "something")
-        billboardSet->setDefaultDimensions(0.0f, 0.0f);
-		billboardSet->setBounds(AxisAlignedBox::BOX_NULL, 0.0f);  
-		billboardSet->setVisible(false);
+      // hide billboardset by default with no boundingbox (blank objects "something")
+      billboardSet->setDefaultDimensions(0.0f, 0.0f);
+      billboardSet->setBounds(AxisAlignedBox::BOX_NULL, 0.0f);
+      billboardSet->setVisible(false);
 
-        // create billboard to draw image on
-        billboard = billboardSet->createBillboard(::Ogre::Vector3::ZERO);
-        billboard->setColour(ColourValue::ZERO);
-           		
-        // create scenenode
-        SceneNode = sceneManager->getRootSceneNode()->createChildSceneNode(ostr_node);
-        SceneNode->attachObject(billboardSet);
+      // create billboard to draw image on
+      billboard = billboardSet->createBillboard(::Ogre::Vector3::ZERO);
+      billboard->setColour(ColourValue::ZERO);
+
+      // create scenenode
+      SceneNode = sceneManager->getRootSceneNode()->createChildSceneNode(ostr_node);
+      SceneNode->attachObject(billboardSet);
 #if DEBUG
-        SceneNode->showBoundingBox(true);
+      SceneNode->showBoundingBox(true);
 #endif
-        // possibly create attached light
-        CreateLight();
 
-        // update scenenode position from datamodel
-        SceneNode->setPosition(Util::ToOgre(Projectile->Position3D));
-	};
+      // possibly create attached light
+      CreateLight();
 
-	ProjectileNode2D::~ProjectileNode2D()
-	{
-		// detach listener
-        Projectile->PropertyChanged -= 
-			gcnew PropertyChangedEventHandler(this, &ProjectileNode2D::OnProjectilePropertyChanged);
-		
-		Projectile->AppearanceChanged -= 
-			gcnew System::EventHandler(this, &ProjectileNode2D::OnProjectileAppearanceChanged);
-		
-        // LIGHT FIRST! 
-        DestroyLight();
+      // update scenenode position from datamodel
+      SceneNode->setPosition(Util::ToOgre(Projectile->Position3D));
+   };
 
-		::Ogre::String& ostr_nodename = 
-			PREFIX_PROJECTILE2D_SCENENODE + ::Ogre::StringConverter::toString(Projectile->ID);
+   ProjectileNode2D::~ProjectileNode2D()
+   {
+      // detach listener
+      Projectile->PropertyChanged -= 
+         gcnew PropertyChangedEventHandler(this, &ProjectileNode2D::OnProjectilePropertyChanged);
 
-        // cleanup scenenode
-        if (sceneManager->hasSceneNode(ostr_nodename))
-            sceneManager->destroySceneNode(ostr_nodename);
-           
-        if (billboardSet != nullptr)
-        {
-            billboardSet->detachFromParent();
-            sceneManager->destroyBillboardSet(billboardSet);            
-        }
+      Projectile->AppearanceChanged -= 
+         gcnew System::EventHandler(this, &ProjectileNode2D::OnProjectileAppearanceChanged);
 
-		billboard		= nullptr;
-		billboardSet	= nullptr;
-		SceneNode		= nullptr;
-	};
+      // LIGHT FIRST! 
+      DestroyLight();
 
-	void ProjectileNode2D::OnProjectilePropertyChanged(Object^ sender, PropertyChangedEventArgs^ e)
-    {
-		if (CLRString::Equals(e->PropertyName, Data::Models::Projectile::PROPNAME_POSITION3D))
-		{
-            // update scenenode position from datamodel
-			SceneNode->setPosition(Util::ToOgre(Projectile->Position3D));
-        }
-    };
+      const ::Ogre::String& ostr_nodename = 
+         PREFIX_PROJECTILE2D_SCENENODE + ::Ogre::StringConverter::toString(Projectile->ID);
 
-	void ProjectileNode2D::OnProjectileAppearanceChanged(Object^ sender, System::EventArgs^ e)
-    {
-		BgfBitmap^ bgfBmp = projectile->ViewerFrame;
+      // cleanup scenenode
+      if (sceneManager->hasSceneNode(ostr_nodename))
+         sceneManager->destroySceneNode(ostr_nodename);
 
-		if (bgfBmp != nullptr)
-		{			
-			// get a unique hash for the current appearance of the object
-			hash->Reset(projectile->AppearanceHash);
-			hash->Step(bgfBmp->Width);
-			hash->Step(bgfBmp->Height);
+      if (billboardSet != nullptr)
+      {
+         billboardSet->detachFromParent();
+         sceneManager->destroyBillboardSet(billboardSet);
+      }
 
-			unsigned int key = hash->Finish();
+      billboard    = nullptr;
+      billboardSet = nullptr;
+      SceneNode    = nullptr;
+   };
+
+   void ProjectileNode2D::OnProjectilePropertyChanged(Object^ sender, PropertyChangedEventArgs^ e)
+   {
+      if (CLRString::Equals(e->PropertyName, Data::Models::Projectile::PROPNAME_POSITION3D))
+      {
+         // update scenenode position from datamodel
+         SceneNode->setPosition(Util::ToOgre(Projectile->Position3D));
+      }
+   };
+
+   void ProjectileNode2D::OnProjectileAppearanceChanged(Object^ sender, System::EventArgs^ e)
+   {
+      BgfBitmap^ bgfBmp = projectile->ViewerFrame;
+
+      if (bgfBmp != nullptr)
+      {
+         // get a unique hash for the current appearance of the object
+         hash->Reset(projectile->AppearanceHash);
+         hash->Step(bgfBmp->Width);
+         hash->Step(bgfBmp->Height);
+
+         unsigned int key = hash->Finish();
             
-			::Ogre::String& keystr  = ::Ogre::StringConverter::toString(key);
-			::Ogre::String& texName = PREFIX_PROJECTILE2D_TEXTURE + keystr;
-			::Ogre::String& matName = PREFIX_PROJECTILE2D_MATERIAL + keystr;
+         const ::Ogre::String& keystr  = ::Ogre::StringConverter::toString(key);
+         const ::Ogre::String& texName = PREFIX_PROJECTILE2D_TEXTURE + keystr;
+         const ::Ogre::String& matName = PREFIX_PROJECTILE2D_MATERIAL + keystr;
 
-			// possibly create texture
-			Util::CreateTextureA8R8G8B8(bgfBmp, texName, ::Ogre::String(TEXTUREGROUP_PROJECTILENODE2D), MIP_DEFAULT);
-			
-			// possibly create material
-			Util::CreateMaterial(
-				matName, texName, 
+         // possibly create texture
+         Util::CreateTextureA8R8G8B8(bgfBmp, texName, ::Ogre::String(TEXTUREGROUP_PROJECTILENODE2D), MIP_DEFAULT);
+
+         // possibly create material
+         Util::CreateMaterial(
+            matName, texName, 
             ::Ogre::String(MATERIALGROUP_PROJECTILENODE2D),
-				nullptr, &::Ogre::Vector4(1.0f, 1.0f, 1.0f, 1.0f), false);
+            nullptr, &::Ogre::Vector4(1.0f, 1.0f, 1.0f, 1.0f), false);
 
-			float scaledwidth = (float)bgfBmp->Width / (float)projectile->Resource->ShrinkFactor;
-			float scaledheight = (float)bgfBmp->Height / (float)projectile->Resource->ShrinkFactor;
+         float scaledwidth = (float)bgfBmp->Width / (float)projectile->Resource->ShrinkFactor;
+         float scaledheight = (float)bgfBmp->Height / (float)projectile->Resource->ShrinkFactor;
             
-			float y = -bgfBmp->YOffset / (float)projectile->Resource->ShrinkFactor;
-			billboard->setPosition(0.0f, y, 0.0f);
+         float y = -bgfBmp->YOffset / (float)projectile->Resource->ShrinkFactor;
+         billboard->setPosition(0.0f, y, 0.0f);
 
-			// apply material and bbox
-			billboardSet->setDefaultDimensions(scaledwidth, scaledheight);        
-			billboardSet->setMaterialName(matName);
-			billboardSet->setVisible(true);	
-		}
-    };
+         // apply material and bbox
+         billboardSet->setDefaultDimensions(scaledwidth, scaledheight);
+         billboardSet->setMaterialName(matName);
+         billboardSet->setVisible(true);
+      }
+   };
 
-	void ProjectileNode2D::CreateLight()
-    {
-		::Ogre::String& ostr_lightname = 
-			PREFIX_PROJECTILE2D_LIGHT + ::Ogre::StringConverter::toString(Projectile->ID);
+   void ProjectileNode2D::CreateLight()
+   {
+      ::Ogre::String& ostr_lightname = 
+         PREFIX_PROJECTILE2D_LIGHT + ::Ogre::StringConverter::toString(Projectile->ID);
 
-        // possibly create a mogre light		
-        Light = Util::CreateLight(Projectile, sceneManager,  ostr_lightname);
+      // possibly create a mogre light
+      Light = Util::CreateLight(Projectile, sceneManager,  ostr_lightname);
 
-        if (Light != nullptr)
-        {
-            // maximum distance we render this light or skip it
-            Light->setRenderingDistance(RemoteNode::MAXLIGHTRENDERDISTANCE);
+      if (Light != nullptr)
+      {
+         // maximum distance we render this light or skip it
+         Light->setRenderingDistance(RemoteNode::MAXLIGHTRENDERDISTANCE);
+         SceneNode->attachObject(Light);
+         Light->setPosition(::Ogre::Vector3(0, 100, 0));
+      }
+   };
 
-            SceneNode->attachObject(Light);
+   void ProjectileNode2D::UpdateLight()
+   {
+      // adjust the light from M59 values (light class extension)
+      if (light)
+         Util::UpdateFromILightOwner(*light, Projectile);
+   };
 
-            Light->setPosition(::Ogre::Vector3(0, 100, 0));
-        }
-    };
+   void ProjectileNode2D::DestroyLight()
+   {
+      const ::Ogre::String& ostr_lightname = 
+         PREFIX_PROJECTILE2D_LIGHT + ::Ogre::StringConverter::toString(Projectile->ID);
 
-	void ProjectileNode2D::UpdateLight()
-    {
-        // adjust the light from M59 values (light class extension)
-        if (light)       		
-            Util::UpdateFromILightOwner(*light, Projectile);                  
-    };
+      if (sceneManager->hasLight(ostr_lightname))
+         sceneManager->destroyLight(ostr_lightname);
 
-	void ProjectileNode2D::DestroyLight()
-    {
-		::Ogre::String& ostr_lightname = 
-			PREFIX_PROJECTILE2D_LIGHT + ::Ogre::StringConverter::toString(Projectile->ID);
-
-        if (sceneManager->hasLight(ostr_lightname))
-            sceneManager->destroyLight(ostr_lightname);
-           
-        light = nullptr;
-    };	
+      light = nullptr;
+   };
 };};
--- Meridian59.Ogre.Client/RemoteNode3DSub.cpp
+++ Meridian59.Ogre.Client/RemoteNode3DSub.cpp
@@ -2,160 +2,159 @@
 
 namespace Meridian59 { namespace Ogre 
 {
-	RemoteNode3DSub::RemoteNode3DSub(Data::Models::SubOverlay^ SubOverlay, ::Ogre::SceneNode* Parent, RemoteNode3D^ RootNode, MeshHotspot^ AttachedHotspot)
-	{
-		this->SubNodes = gcnew List<RemoteNode3DSub^>();
-        this->SubOverlay = SubOverlay;
-        this->Parent = Parent;
-        this->RootNode = RootNode;
-        this->AttachedHotspot = AttachedHotspot;
+   RemoteNode3DSub::RemoteNode3DSub(Data::Models::SubOverlay^ SubOverlay, ::Ogre::SceneNode* Parent, RemoteNode3D^ RootNode, MeshHotspot^ AttachedHotspot)
+   {
+      this->SubNodes        = gcnew List<RemoteNode3DSub^>();
+      this->SubOverlay      = SubOverlay;
+      this->Parent          = Parent;
+      this->RootNode        = RootNode;
+      this->AttachedHotspot = AttachedHotspot;
 
-        CLRString^ resource = SubOverlay->Name->Replace(FileExtensions::BGF, FileExtensions::XML);
-        ::Ogre::String& ostr_resource = StringConvert::CLRToOgre(resource);
+      CLRString^ resource           = SubOverlay->Name->Replace(FileExtensions::BGF, FileExtensions::XML);
+      ::Ogre::String& ostr_resource = StringConvert::CLRToOgre(resource);
 
-		ResourceGroupManager* resMan = ResourceGroupManager::getSingletonPtr();
-                  
-        if (resMan->resourceExists(RESOURCEGROUPMODELS, ostr_resource))
-        {
-            // load info
-			Model3DInfo = gcnew ::Meridian59::Ogre::Model3DInfo(ostr_resource, RESOURCEGROUPMODELS);
-                    
-            // create 3D object
-            CreateMesh();
-            CreateParticles();
-            CreateSubNodes();
-        }
-	};
+      ResourceGroupManager& resMan = ResourceGroupManager::getSingleton();
 
-	CLRString^ RemoteNode3DSub::ID::get() 
-	{ 
-		return RootNode->RoomObject->ID + "_" + SubOverlay->HotSpot + "_" + SubOverlay->Name; 
-	};
+      if (resMan.resourceExists(RESOURCEGROUPMODELS, ostr_resource))
+      {
+         // load info
+         Model3DInfo = gcnew ::Meridian59::Ogre::Model3DInfo(ostr_resource, RESOURCEGROUPMODELS);
 
-	void RemoteNode3DSub::CreateMesh()
-    {
-        // get info from datamodel
-        MeshInfo^ info = Model3DInfo->MeshInfo;
+         // create 3D object
+         CreateMesh();
+         CreateParticles();
+         CreateSubNodes();
+      }
+   };
 
-		::Ogre::String& ostr_name = StringConvert::CLRToOgre(PREFIX_REMOTENODE_ENTITY + ID);
-		::Ogre::String& ostr_name2 = StringConvert::CLRToOgre(PREFIX_REMOTENODE_SCENENODE + ID);
+   CLRString^ RemoteNode3DSub::ID::get() 
+   { 
+      return RootNode->RoomObject->ID + "_" + SubOverlay->HotSpot + "_" + SubOverlay->Name; 
+   };
 
-        // create entity
-        Entity = RootNode->SceneManager->createEntity(
-            ostr_name, 
-            *Model3DInfo->MeshInfo->MeshFile,
-            RESOURCEGROUPMODELS);
+   void RemoteNode3DSub::CreateMesh()
+   {
+      // get info from datamodel
+      MeshInfo^ info = Model3DInfo->MeshInfo;
 
-        Entity->setCastShadows(info->CastShadows);
+      ::Ogre::String& ostr_name = StringConvert::CLRToOgre(PREFIX_REMOTENODE_ENTITY + ID);
+      ::Ogre::String& ostr_name2 = StringConvert::CLRToOgre(PREFIX_REMOTENODE_SCENENODE + ID);
 
-        // create scenenode
-        SceneNode = Parent->createChildSceneNode(ostr_name2);
-        SceneNode->attachObject(Entity);
-                       
-        SceneNode->setPosition(*AttachedHotspot->Position);
-		SceneNode->setOrientation(*info->Orientation);
-        SceneNode->scale(*info->Scale);
-        SceneNode->setInitialState();
-    };
+      // create entity
+      Entity = RootNode->SceneManager->createEntity(
+         ostr_name, 
+         *Model3DInfo->MeshInfo->MeshFile,
+         RESOURCEGROUPMODELS);
 
-	void RemoteNode3DSub::CreateSubNodes()
-    {
-        // process attached subnodes/suboverlays
-        for each (Data::Models::SubOverlay^ subOverlay in RootNode->RoomObject->SubOverlays)
-        {
-            // get all subnodes belonging to this subnode
-            MeshHotspot^ hotspot = MeshHotspot::Find(subOverlay->HotSpot, Model3DInfo->Hotspots);
-            if (hotspot != nullptr)
-                SubNodes->Add(gcnew RemoteNode3DSub(subOverlay, SceneNode, RootNode, hotspot));
-        }
-    };
+      Entity->setCastShadows(info->CastShadows);
 
-	void RemoteNode3DSub::CreateParticles()
-    {          
-        ParticleSystems = new std::vector<::ParticleUniverse::ParticleSystem*>(Model3DInfo->ParticleSystemsData->Length);
+      // create scenenode
+      SceneNode = Parent->createChildSceneNode(ostr_name2);
+      SceneNode->attachObject(Entity);
 
-        // process each
-        for (unsigned int i = 0; i < ParticleSystems->size(); i++)
-        {
-            // get info from datamodel
-            ParticleSystemInfo^ info = (ParticleSystemInfo^)Model3DInfo->ParticleSystemsData[i];
+      SceneNode->setPosition(*AttachedHotspot->Position);
+      SceneNode->setOrientation(*info->Orientation);
+      SceneNode->scale(*info->Scale);
+      SceneNode->setInitialState();
+   };
 
-			if (!info->TemplateValue || !info->Position)
-				continue;
+   void RemoteNode3DSub::CreateSubNodes()
+   {
+      // process attached subnodes/suboverlays
+      for each (Data::Models::SubOverlay^ subOverlay in RootNode->RoomObject->SubOverlays)
+      {
+         // get all subnodes belonging to this subnode
+         MeshHotspot^ hotspot = MeshHotspot::Find(subOverlay->HotSpot, Model3DInfo->Hotspots);
+         if (hotspot != nullptr)
+            SubNodes->Add(gcnew RemoteNode3DSub(subOverlay, SceneNode, RootNode, hotspot));
+      }
+   };
 
-			::Ogre::String& ostr_particleid = StringConvert::CLRToOgre(ID);
-			::Ogre::String& ostr_particlename = ostr_particleid.append("_").append(*info->Name);
+   void RemoteNode3DSub::CreateParticles()
+   {
+      ParticleSystems = new std::vector<::ParticleUniverse::ParticleSystem*>(Model3DInfo->ParticleSystemsData->Length);
 
-           ::ParticleUniverse::ParticleSystemManager* particleMan =
-				::ParticleUniverse::ParticleSystemManager::getSingletonPtr();
+      // process each
+      for (unsigned int i = 0; i < ParticleSystems->size(); i++)
+      {
+         // get info from datamodel
+         ParticleSystemInfo^ info = (ParticleSystemInfo^)Model3DInfo->ParticleSystemsData[i];
 
-			::ParticleUniverse::ParticleSystem* particleSystem = 
-				particleMan->createParticleSystem(ostr_particlename + *info->TemplateValue, RootNode->SceneManager);
-			
-			for (size_t j = 0; j < particleSystem->getNumTechniques(); j++)
-				particleSystem->getTechnique(j)->position = *info->Position;
-			
-            // attach particlesystem to parent scenenode
-            SceneNode->attachObject(particleSystem);
+         if (!info->TemplateValue || !info->Position)
+            continue;
 
-			// start particles
-			particleSystem->start();
+         ::Ogre::String& ostr_particleid = StringConvert::CLRToOgre(ID);
+         ::Ogre::String& ostr_particlename = ostr_particleid.append("_").append(*info->Name);
 
-            // save reference to this particle system
-            ParticleSystems->at(i) = particleSystem;
-        }
-    };
+         ::ParticleUniverse::ParticleSystemManager* particleMan =
+            ::ParticleUniverse::ParticleSystemManager::getSingletonPtr();
 
-	void RemoteNode3DSub::Destroy()
-    {
-		::Ogre::String& ostr_scenenode = StringConvert::CLRToOgre(PREFIX_REMOTENODE_SCENENODE + ID);
-		::Ogre::String& ostr_entity = StringConvert::CLRToOgre(PREFIX_REMOTENODE_ENTITY + ID);
-		
-        // cleanup scenenode
-        if (RootNode->SceneManager->hasSceneNode(ostr_scenenode))
-            RootNode->SceneManager->destroySceneNode(ostr_scenenode);
+         ::ParticleUniverse::ParticleSystem* particleSystem = 
+         particleMan->createParticleSystem(ostr_particlename + *info->TemplateValue, RootNode->SceneManager);
 
-        if (SceneNode != nullptr)
-        {
-            //SceneNode.Dispose();
-            SceneNode = nullptr;
-        }
+         for (size_t j = 0; j < particleSystem->getNumTechniques(); j++)
+            particleSystem->getTechnique(j)->position = *info->Position;
 
-        // cleanup entity
-        if (RootNode->SceneManager->hasEntity(ostr_entity))
-            RootNode->SceneManager->destroyEntity(ostr_entity);
+         // attach particlesystem to parent scenenode
+         SceneNode->attachObject(particleSystem);
 
-        if (Entity != nullptr)
-        {
-            //Entity.Dispose();
-            Entity = nullptr;
-        }
+         // start particles
+         particleSystem->start();
 
-        if (ParticleSystems != nullptr)
-        {
-            // cleanup particle systems                
-            for(unsigned int i=0; i<ParticleSystems->size(); i++)
-            {
-                ::ParticleUniverse::ParticleSystem* particleSystem = ParticleSystems->at(i);
+         // save reference to this particle system
+         ParticleSystems->at(i) = particleSystem;
+      }
+   };
 
-				::ParticleUniverse::ParticleSystemManager* particleMan = 
-					::ParticleUniverse::ParticleSystemManager::getSingletonPtr();
+   void RemoteNode3DSub::Destroy()
+   {
+      ::Ogre::String& ostr_scenenode = StringConvert::CLRToOgre(PREFIX_REMOTENODE_SCENENODE + ID);
+      ::Ogre::String& ostr_entity = StringConvert::CLRToOgre(PREFIX_REMOTENODE_ENTITY + ID);
 
-                particleMan->destroyParticleSystem(particleSystem, RootNode->SceneManager);
-            }
+      // cleanup scenenode
+      if (RootNode->SceneManager->hasSceneNode(ostr_scenenode))
+         RootNode->SceneManager->destroySceneNode(ostr_scenenode);
 
-			delete ParticleSystems;
+      if (SceneNode != nullptr)
+      {
+         //SceneNode.Dispose();
+         SceneNode = nullptr;
+      }
 
-            ParticleSystems = nullptr;
-        }
+      // cleanup entity
+      if (RootNode->SceneManager->hasEntity(ostr_entity))
+         RootNode->SceneManager->destroyEntity(ostr_entity);
 
-        if (SubNodes != nullptr)
-        {
-            for each (RemoteNode3DSub^ subNode in SubNodes)
-                subNode->Destroy();
+      if (Entity != nullptr)
+      {
+         //Entity.Dispose();
+         Entity = nullptr;
+      }
 
-            SubNodes->Clear();
-            SubNodes = nullptr;
-        }
-    };
+      if (ParticleSystems != nullptr)
+      {
+         // cleanup particle systems
+         for(unsigned int i=0; i<ParticleSystems->size(); i++)
+         {
+            ::ParticleUniverse::ParticleSystem* particleSystem = ParticleSystems->at(i);
+
+            ::ParticleUniverse::ParticleSystemManager* particleMan = 
+               ::ParticleUniverse::ParticleSystemManager::getSingletonPtr();
+
+            particleMan->destroyParticleSystem(particleSystem, RootNode->SceneManager);
+         }
+
+         delete ParticleSystems;
+         ParticleSystems = nullptr;
+      }
+
+      if (SubNodes != nullptr)
+      {
+         for each (RemoteNode3DSub^ subNode in SubNodes)
+               subNode->Destroy();
+
+         SubNodes->Clear();
+         SubNodes = nullptr;
+      }
+   };
 };};
