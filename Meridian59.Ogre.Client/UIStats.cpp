0ce09da4e834ee9fb0394247dcdf7b1a9e2f391b
clint.banzhaf <clint.banzhaf@CLINT>
2017-06-07 02:16:38 +0200

    Add object cache item counts to stats window

--- Meridian59.Ogre.Client/UIStats.cpp
+++ Meridian59.Ogre.Client/UIStats.cpp
@@ -43,58 +43,85 @@ namespace Meridian59 { namespace Ogre
       if (!Window->isVisible())
          return;
 
+      // bytes of 1 MB
+      const size_t MB = 1024 * 1024;
+
       OgreClient^ client = OgreClient::Singleton;
       MaterialManager& matMan = MaterialManager::getSingleton();
       TextureManager& texMan = TextureManager::getSingleton();
       //ResourceGroupManager& resGrpMan = ResourceGroupManager::getSingleton();
-      const size_t MB = 1024 * 1024;
 
+      /*unsigned int numViewportBatches = client->Viewport->_getNumRenderedBatches();
+      unsigned int numViewportFaces = client->Viewport->_getNumRenderedFaces();
+      unsigned int numViewportInvisBatches = client->ViewportInvis->_getNumRenderedBatches();
+      unsigned int numViewportInvisFaces = client->ViewportInvis->_getNumRenderedFaces();*/
+
+      //CEGUI::ImageManager& imgMan = CEGUI::ImageManager::getSingleton();
+      //CEGUI::uint numCEGUIImages = imgMan.getImageCount();
+
+      //----------------------------------------------------------------------------------------------------------//
       // RenderWindow
-      size_t numRenderWindowBatches   = client->RenderWindow->getBatchCount();
+      size_t numRenderWindowBatches = client->RenderWindow->getBatchCount();
       size_t numRenderWindowTriangles = client->RenderWindow->getTriangleCount();
 
+      RenderWindowBatches->setText(CEGUI::PropertyHelper<size_t>::toString(numRenderWindowBatches));
+      RenderWindowTriangles->setText(CEGUI::PropertyHelper<size_t>::toString(numRenderWindowTriangles));
+
+      //----------------------------------------------------------------------------------------------------------//
       // RenderSystem
       unsigned int numRenderSystemBatches = client->RenderSystem->_getBatchCount();
       unsigned int numRenderSystemFaces = client->RenderSystem->_getFaceCount();
       unsigned int numRenderSystemVertices = client->RenderSystem->_getVertexCount();
 
+      RenderSystemBatches->setText(CEGUI::PropertyHelper<double>::toString(numRenderSystemBatches));
+      RenderSystemFaces->setText(CEGUI::PropertyHelper<double>::toString(numRenderSystemFaces));
+      RenderSystemVertices->setText(CEGUI::PropertyHelper<double>::toString(numRenderSystemVertices));
 
-      unsigned int numViewportBatches = client->Viewport->_getNumRenderedBatches();
-      unsigned int numViewportFaces = client->Viewport->_getNumRenderedFaces();
-      unsigned int numViewportInvisBatches = client->ViewportInvis->_getNumRenderedBatches();
-      unsigned int numViewportInvisFaces = client->ViewportInvis->_getNumRenderedFaces();
-
+      //----------------------------------------------------------------------------------------------------------//
       // OgreMem
-      size_t memTextures  = texMan.getMemoryUsage() / MB;
+      size_t memTextures = texMan.getMemoryUsage() / MB;
       size_t memMaterials = matMan.getMemoryUsage() / MB;
 
-      CEGUI::ImageManager& imgMan = CEGUI::ImageManager::getSingleton();
-      CEGUI::uint numCEGUIImages = imgMan.getImageCount();
-      
+      OgreMemTextures->setText(CEGUI::PropertyHelper<size_t>::toString(memTextures).append(" MB"));
+      OgreMemMaterials->setText(CEGUI::PropertyHelper<size_t>::toString(memMaterials).append(" MB"));
+
+      //----------------------------------------------------------------------------------------------------------//
+      // Other
       size_t numLightsFrustum = client->SceneManager->_getLightsAffectingFrustum().size();
       unsigned int numRoomSections = ControllerRoom::RoomManualObject->getNumSections();
+
+      OgreFrustumLights->setText(CEGUI::PropertyHelper<size_t>::toString(numLightsFrustum));
+      OgreRoomSections->setText(CEGUI::PropertyHelper<size_t>::toString(numRoomSections));
+
+      //----------------------------------------------------------------------------------------------------------//
+      // Object Caches
 
       unsigned int memCEGUIObjectCacheRoom = ImageComposerCEGUI<RoomObject^>::Cache::CacheSize / MB;
       unsigned int memCEGUIObjectCacheObject = ImageComposerCEGUI<ObjectBase^>::Cache::CacheSize / MB;
       unsigned int memCEGUIObjectCacheInventory = ImageComposerCEGUI<InventoryObject^>::Cache::CacheSize / MB;
       unsigned int memOgreRoomObjectCache = ImageComposerOgre<RoomObject^>::Cache::CacheSize / MB;
 
-      RenderWindowBatches->setText(CEGUI::PropertyHelper<size_t>::toString(numRenderWindowBatches));
-      RenderWindowTriangles->setText(CEGUI::PropertyHelper<size_t>::toString(numRenderWindowTriangles));
+      int numCEGUIObjectCacheRoom = ImageComposerCEGUI<RoomObject^>::Cache::Count;
+      int numCEGUIObjectCacheObject = ImageComposerCEGUI<ObjectBase^>::Cache::Count;
+      int numCEGUIObjectCacheInventory = ImageComposerCEGUI<InventoryObject^>::Cache::Count;
+      int numOgreRoomObjectCache = ImageComposerOgre<RoomObject^>::Cache::Count;
 
-      RenderSystemBatches->setText(CEGUI::PropertyHelper<double>::toString(numRenderSystemBatches));
-      RenderSystemFaces->setText(CEGUI::PropertyHelper<double>::toString(numRenderSystemFaces));
-      RenderSystemVertices->setText(CEGUI::PropertyHelper<double>::toString(numRenderSystemVertices));
+      CEGUIObjectCacheRoom->setText(
+         CEGUI::PropertyHelper<int>::toString(numCEGUIObjectCacheRoom) + " (" +
+         CEGUI::PropertyHelper<unsigned int>::toString(memCEGUIObjectCacheRoom) + " MB)");
 
-      OgreMemTextures->setText(CEGUI::PropertyHelper<size_t>::toString(memTextures).append(" MB"));
-      OgreMemMaterials->setText(CEGUI::PropertyHelper<size_t>::toString(memMaterials).append(" MB"));
+      CEGUIObjectCacheObject->setText(
+         CEGUI::PropertyHelper<int>::toString(numCEGUIObjectCacheObject) + " (" +
+         CEGUI::PropertyHelper<unsigned int>::toString(memCEGUIObjectCacheObject) + " MB)");
 
-      CEGUIObjectCacheRoom->setText(CEGUI::PropertyHelper<size_t>::toString(memCEGUIObjectCacheRoom).append(" MB"));
-      CEGUIObjectCacheObject->setText(CEGUI::PropertyHelper<size_t>::toString(memCEGUIObjectCacheObject).append(" MB"));
-      CEGUIObjectCacheInventory->setText(CEGUI::PropertyHelper<size_t>::toString(memCEGUIObjectCacheInventory).append(" MB"));
-      OgreRoomObjectCache->setText(CEGUI::PropertyHelper<size_t>::toString(memOgreRoomObjectCache).append(" MB"));
+      CEGUIObjectCacheInventory->setText(
+         CEGUI::PropertyHelper<int>::toString(numCEGUIObjectCacheInventory) + " (" +
+         CEGUI::PropertyHelper<unsigned int>::toString(memCEGUIObjectCacheInventory) + " MB)");
 
-      OgreFrustumLights->setText(CEGUI::PropertyHelper<size_t>::toString(numLightsFrustum));
-      OgreRoomSections->setText(CEGUI::PropertyHelper<size_t>::toString(numRoomSections));
+      OgreRoomObjectCache->setText(
+         CEGUI::PropertyHelper<int>::toString(numOgreRoomObjectCache) + " (" +
+         CEGUI::PropertyHelper<unsigned int>::toString(memOgreRoomObjectCache) + " MB)");
+
+      //----------------------------------------------------------------------------------------------------------//
    };
 };};
