f9a31e38ac601a66ce3bd58b650d84f4c8a2db1e
clint.banzhaf <clint.banzhaf@CLINT>
2017-06-17 19:11:11 +0200

    Increment AdminUI version

--- Meridian59.AdminUI/Properties/AssemblyInfo.cs
+++ Meridian59.AdminUI/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("175ff980-037a-49ab-b948-ad58abd74716")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.1")]
+[assembly: AssemblyFileVersion("1.3.0.1")]
