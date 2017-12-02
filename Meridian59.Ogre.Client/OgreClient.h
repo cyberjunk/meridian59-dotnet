01211dd404b839434ee65850816ad7450c861b77
skittles1 <tmx883@gmail.com>
2017-12-02 10:55:21 +0800

    Increment corelib and ogre versions.

--- Meridian59.Ogre.Client/OgreClient.h
+++ Meridian59.Ogre.Client/OgreClient.h
@@ -265,7 +265,7 @@ namespace Meridian59 { namespace Ogre
 
       property unsigned char AppVersionMinor
       { 
-         public: virtual unsigned char get() override { return 17; }
+         public: virtual unsigned char get() override { return 18; }
       };
 
       property ::Ogre::Root* Root 
