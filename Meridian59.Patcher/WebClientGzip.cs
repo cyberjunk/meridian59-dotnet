1f181ce79dca57c2c2b4b5c204a880daff88bbc9
clint.banzhaf <clint.banzhaf@CLINT>
2018-08-24 02:28:12 +0200

    Add missing file

---
+++ Meridian59.Patcher/WebClientGzip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace Meridian59.Patcher
+{
+   /// <summary>
+   /// WebClient with 'Accept-Encoding: gzip'
+   /// </summary>
+   public class WebClientGzip : WebClient
+   {
+      public WebClientGzip() : base()
+      {
+         this.Headers[HttpRequestHeader.AcceptEncoding] = "gzip";
+      }
+
+      protected override WebRequest GetWebRequest(Uri address)
+      {
+         HttpWebRequest request = base.GetWebRequest(address) as HttpWebRequest;
+         request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+         return request;
+      }
+   }
+}
