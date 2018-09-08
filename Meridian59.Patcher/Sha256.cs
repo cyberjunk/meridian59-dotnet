4a2f819bf2ea3a32b7659e32f7febf91bd8b6409
clint.banzhaf <clint.banzhaf@CLINT>
2018-09-08 19:15:10 +0200

    Cleanup Sha256 round 2

--- Meridian59.Patcher/Sha256.cs
+++ Meridian59.Patcher/Sha256.cs
@@ -1,6 +1,5 @@
 ﻿
 using System;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 
@@ -11,7 +10,7 @@ namespace Meridian59.Patcher
    /// </summary>
    public class Sha256
    {
-      private static readonly UInt32[] K = new UInt32[64] {
+      private static readonly uint[] K = new uint[64] {
             0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
             0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
             0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
@@ -22,29 +21,30 @@ namespace Meridian59.Patcher
             0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
         };
 
-      private static readonly UInt32[] SEED = new UInt32[8] {
+      private static readonly uint[] SEED = new uint[8] {
             0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
         };
 
       //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      private static UInt32 ROTL(UInt32 x, byte n) { return (x << n) | (x >> (32 - n)); }
-      private static UInt32 ROTR(UInt32 x, byte n) { return (x >> n) | (x << (32 - n)); }
-      private static UInt32 Ch(UInt32 x, UInt32 y, UInt32 z) { return (x & y) ^ ((~x) & z); }
-      private static UInt32 Maj(UInt32 x, UInt32 y, UInt32 z) { return (x & y) ^ (x & z) ^ (y & z); }
-      private static UInt32 Sigma0(UInt32 x) { return ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22); }
-      private static UInt32 Sigma1(UInt32 x) { return ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25); }
-      private static UInt32 sigma0(UInt32 x) { return ROTR(x, 7) ^ ROTR(x, 18) ^ (x >> 3); }
-      private static UInt32 sigma1(UInt32 x) { return ROTR(x, 17) ^ ROTR(x, 19) ^ (x >> 10); }
+      private static uint ROTL(uint x, byte n) { return (x << n) | (x >> (32 - n)); }
+      private static uint ROTR(uint x, byte n) { return (x >> n) | (x << (32 - n)); }
+      private static uint Ch(uint x, uint y, uint z) { return (x & y) ^ ((~x) & z); }
+      private static uint Maj(uint x, uint y, uint z) { return (x & y) ^ (x & z) ^ (y & z); }
+      private static uint Sigma0(uint x) { return ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22); }
+      private static uint Sigma1(uint x) { return ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25); }
+      private static uint sigma0(uint x) { return ROTR(x, 7) ^ ROTR(x, 18) ^ (x >> 3); }
+      private static uint sigma1(uint x) { return ROTR(x, 17) ^ ROTR(x, 19) ^ (x >> 10); }
 
       //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      private UInt32[] H = new UInt32[8];
-      private byte[] pending_block = new byte[64];
-      private UInt32[] uint_buffer = new UInt32[16];
+      private readonly uint[] H = new uint[8];
+      private readonly uint[] W = new uint[64];
+      private readonly byte[] pending_block = new byte[64];
+      private readonly uint[] uint_buffer = new uint[16];
+      private readonly byte[] buf = new byte[8192];
       private uint pending_block_off;
       private UInt64 bits_processed;
-      private byte[] buf = new byte[8192];
 
       //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -67,37 +67,34 @@ namespace Meridian59.Patcher
          bits_processed = 0;
       }
 
-      private void ProcessBlock(UInt32[] M)
+      private void ProcessBlock(uint[] M)
       {
          Debug.Assert(M.Length == 16);
 
          // 1. Prepare the message schedule (W[t]):
-         UInt32[] W = new UInt32[64];
-         for (int t = 0; t < 16; ++t)
-         {
-            W[t] = M[t];
-         }
+         Array.Clear(W, 0, W.Length); //needed?
 
-         for (int t = 16; t < 64; ++t)
-         {
+         for (uint t = 0; t < 16; ++t)
+            W[t] = M[t];
+
+         for (uint t = 16; t < 64; ++t)
             W[t] = sigma1(W[t - 2]) + W[t - 7] + sigma0(W[t - 15]) + W[t - 16];
-         }
 
          // 2. Initialize the eight working variables with the (i-1)-st hash value:
-         UInt32 a = H[0],
-                b = H[1],
-                c = H[2],
-                d = H[3],
-                e = H[4],
-                f = H[5],
-                g = H[6],
-                h = H[7];
+         uint a = H[0],
+              b = H[1],
+              c = H[2],
+              d = H[3],
+              e = H[4],
+              f = H[5],
+              g = H[6],
+              h = H[7];
 
          // 3. For t=0 to 63:
-         for (int t = 0; t < 64; ++t)
+         for (uint t = 0; t < 64; ++t)
          {
-            UInt32 T1 = h + Sigma1(e) + Ch(e, f, g) + K[t] + W[t];
-            UInt32 T2 = Sigma0(a) + Maj(a, b, c);
+            uint T1 = h + Sigma1(e) + Ch(e, f, g) + K[t] + W[t];
+            uint T2 = Sigma0(a) + Maj(a, b, c);
             h = g;
             g = f;
             f = e;
@@ -109,14 +106,14 @@ namespace Meridian59.Patcher
          }
 
          // 4. Compute the intermediate hash value H:
-         H[0] = a + H[0];
-         H[1] = b + H[1];
-         H[2] = c + H[2];
-         H[3] = d + H[3];
-         H[4] = e + H[4];
-         H[5] = f + H[5];
-         H[6] = g + H[6];
-         H[7] = h + H[7];
+         H[0] += a;
+         H[1] += b;
+         H[2] += c;
+         H[3] += d;
+         H[4] += e;
+         H[5] += f;
+         H[6] += g;
+         H[7] += h;
       }
 
       private void AddData(byte[] data, uint offset, uint len)
@@ -156,12 +153,7 @@ namespace Meridian59.Patcher
          }
       }
 
-      private byte[] GetHash()
-      {
-         return toByteArray(GetHashUInt32());
-      }
-
-      private ReadOnlyCollection<UInt32> GetHashUInt32()
+      private byte[] Finish()
       {
          UInt64 size_temp = bits_processed;
 
@@ -174,6 +166,7 @@ namespace Meridian59.Patcher
 
          // 0-initialized
          byte[] padding = new byte[available_space];
+
          // Insert lenght uint64
          for (uint i = 1; i <= 8; ++i)
          {
@@ -185,7 +178,7 @@ namespace Meridian59.Patcher
 
          Debug.Assert(pending_block_off == 0);
 
-         return Array.AsReadOnly(H);
+         return toByteArray(H);
       }
 
       /// <summary>
@@ -208,7 +201,7 @@ namespace Meridian59.Patcher
          }
          while (bytes_read == 8192);
 
-         byte[] hash = GetHash();
+         byte[] hash = Finish();
 
          Reset();
 
@@ -217,20 +210,24 @@ namespace Meridian59.Patcher
 
       //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      private static void toUintArray(byte[] src, UInt32[] dest)
+      private static void toUintArray(byte[] src, uint[] dest)
       {
          for (uint i = 0, j = 0; i < dest.Length; ++i, j += 4)
          {
-            dest[i] = ((UInt32)src[j + 0] << 24) | ((UInt32)src[j + 1] << 16) | ((UInt32)src[j + 2] << 8) | ((UInt32)src[j + 3]);
+            dest[i] = 
+               ((uint)src[j + 0] << 24) | 
+               ((uint)src[j + 1] << 16) | 
+               ((uint)src[j + 2] << 8)  | 
+               ((uint)src[j + 3]);
          }
       }
 
-      private static byte[] toByteArray(ReadOnlyCollection<UInt32> src)
+      private static byte[] toByteArray(uint[] src)
       {
-         byte[] dest = new byte[src.Count * 4];
+         byte[] dest = new byte[src.Length * 4];
          int pos = 0;
 
-         for (int i = 0; i < src.Count; ++i)
+         for (int i = 0; i < src.Length; ++i)
          {
             dest[pos++] = (byte)(src[i] >> 24);
             dest[pos++] = (byte)(src[i] >> 16);
