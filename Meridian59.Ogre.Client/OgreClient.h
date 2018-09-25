c99c262efae73fab386fb6d10392b9ac0d37cd46
Clint Banzhaf <clint.banzhaf@zds.de>
2018-09-25 13:32:59 +0200

    Increment OgreClient version

--- Meridian59.Ogre.Client/OgreClient.h
+++ Meridian59.Ogre.Client/OgreClient.h
@@ -278,7 +278,7 @@ namespace Meridian59 { namespace Ogre
 
       property unsigned char AppVersionMinor
       { 
-         public: virtual unsigned char get() override { return 21; }
+         public: virtual unsigned char get() override { return 22; }
       };
 
       property ::Ogre::Root* Root 
