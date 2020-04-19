b95259705ee321ea2cbf6f037288172b29672d33
skittles1 <tmx883@gmail.com>
2020-04-19 19:25:21 +0800

    Increment ogre client/core lib version.
    Increment the versions for the recent changes.

--- Meridian59.Ogre.Client/AssemblyInfo.cpp
+++ Meridian59.Ogre.Client/AssemblyInfo.cpp
@@ -14,6 +14,6 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyCopyrightAttribute("GPL 3.0")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
-[assembly:AssemblyVersionAttribute("1.0.5.11")];
+[assembly:AssemblyVersionAttribute("1.0.5.12")];
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
